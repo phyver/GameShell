5f8a312b64c184fff1ec16cfb01b2eb6ee9e6a80
Pierre Hyvernat <pierre.hyvernat@univ-savoie.fr>
2017-03-25 16:08:50 +0100

    un peu de travail sur archive.sh

--- bin/archive.sh
+++ bin/archive.sh
@@ -9,19 +9,33 @@ NAME="GameShell"
 TMP_DIR=$(mktemp -d)
 mkdir $TMP_DIR/$NAME
 
+MISSIONS="??_*"
 
+# copy source files
 cp --archive $GASH_BASE/start.sh $GASH_BASE/bin $GASH_BASE/lib $TMP_DIR/$NAME
 
+# copy missions
 mkdir $TMP_DIR/$NAME/missions
-cp --archive $GASH_BASE/missions/* $TMP_DIR/$NAME/missions
+cd $GASH_BASE/missions
+N=1
+for m in $MISSIONS
+do
+    N=$(echo -n "0000$N" | tail -c 2)
+    MISSION_DIR=$TMP_DIR/$NAME/missions/${N}_${m#*_}
+    mkdir $MISSION_DIR
+    cp --archive $m/* $MISSION_DIR
+    N=$((10#$N + 1))
+done
+
 # remove auto.sh files
 find $TMP_DIR/$NAME/missions -name auto.sh | xargs rm -f
 
-# admin password
+# change admin password
 ADMIN_PASSWD="bbq"
 ADMIN_HASH=$(checksum $ADMIN_PASSWD)
 sed -i "s/^export ADMIN_HASH='[0-9a-f]*'$/export ADMIN_HASH='$ADMIN_HASH'/" $TMP_DIR/$NAME/lib/utils.sh
 
+# create archive
 cd $TMP_DIR
 tar --create --file $NAME.tar $NAME
 mv $NAME.tar $GASH_BASE/../
