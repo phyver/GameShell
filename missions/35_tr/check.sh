7dace0ecc6a9687c87002a608a811d0c82be82e0
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-03-10 17:26:51 +0100

    mission 35, envoyée par un étudiant, il y a bien longtemps
    (Tiemen Duvillard)

---
+++ missions/35_tr/check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Quel est la clé qui fait apparaitre le coffre de Merlin ? "
+read dcode
+if [ "$dcode" = "$_secret_key" ]
+then
+    unset dcode _secret_key
+    mkdir -p "$GASH_HOME/Chateau/Cave/Coffre_de_merlin"                       # je met le coffre
+    cp "$GASH_MISSIONS"/*_tr/recette_secrete "$GASH_HOME/Chateau/Cave/Coffre_de_merlin" # et son contenu (cela ne sert à rien pour la mission, mais si 
+    true                                                                    # l'utilisateur à la curiosité d'aller voir, il y aura qqch)
+else
+    echo "Ce n'est pas la bonne clé.."
+    unset dcode _secret_key
+    false
+fi
+
---
+++ missions/35_tr/init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# mission soumise par Tiemen Duvillard
+
+# un chaine aléatoire
+ab=okduygvjilelctawqnucevoxhzdmkabbnqsyfpfjhtwrmgirspzx
+
+# je choisi une clé en prenant un bout au hasard de la chaine ci dessus
+_secret_key=${ab:( $RANDOM % ((${#ab}) -4) ):4}
+
+# un décalage pas trop grand (entre -6 et +6) pour que la recherche exhaustive ne soit pas trop longue
+decalage=7
+while [ "$decalage" -eq 7 ]
+do
+    decalage=$((RANDOM %13)) # je choisi un décalage de cesar au hasard
+done
+
+ch=tuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrs
+C=${ch:$decalage:26} # je crée mon alphabet d'arrivée en tronquand la chaine ci dessus et en m'aidant du décalage de cesar
+
+echo "voici mon testament :
+je vous legue mon coffre et son contenu.
+le coffre se situe dans la cave.
+la cle pour le faire apparaitre est : $_secret_key
+Merlin l'Enchanteur" | tr "a-z" $C > $GASH_HOME/Chateau/Batiment_principal/Bibliotheque/.message_secret # je crée le fichier dans la bibliotheque de Merlin
+
+unset decalage ch C ab # je supprime mes variables, sauf _secret_key, dont on a besoin pour le gash check
