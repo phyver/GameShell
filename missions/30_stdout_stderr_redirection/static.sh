1697c074bea4b9cdf4fe498643b8f4906de9b46a
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-03-24 13:41:21 +0100

    vérification que gcc existe pour la mission 30

--- missions/30_stdout_stderr_redirection/static.sh
+++ missions/30_stdout_stderr_redirection/static.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mkdir -p "$GASH_HOME/Chateau/Observatoire"
-gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
-
+if ! gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
+then
+    echo "ERREUR lors de la commande"
+    echo "gcc -o \"$GASH_HOME/Chateau/Observatoire/merlin\" \"$GASH_MISSIONS\"/*_stdout_stderr_redirection/merlin.c"
+    exit
+fi
