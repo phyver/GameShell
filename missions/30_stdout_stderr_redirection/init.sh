e74839ec0533160bbd68168cbff1dbe8825519e0
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-03-26 12:29:43 +0100

    toujours regenerer merlin dans la mission 30

--- missions/30_stdout_stderr_redirection/init.sh
+++ missions/30_stdout_stderr_redirection/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 mkdir -p "$GASH_HOME/Chateau/Observatoire"
-[ -f "$GASH_HOME/Chateau/Observatoire/merlin" ] || gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
+# certains se débrouillent pour écraser merlin ! Il faut mieux le regénérer,
+# même si le fichier existe !
+gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
 
