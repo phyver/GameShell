240c5cf7c6013b1c8b2f18adca193c1ddcf57b5e
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-03-31 15:42:25 +0200

    pour la mission 30, utilise un script python si gcc n'est pas trouvé

--- missions/30_stdout_stderr_redirection/init.sh
+++ missions/30_stdout_stderr_redirection/init.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 mkdir -p "$GASH_HOME/Chateau/Observatoire"
+
 # certains se débrouillent pour écraser merlin ! Il faut mieux le regénérer,
 # même si le fichier existe !
-gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
+if command -v gcc > /dev/null
+then
+    gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
+elif command -v python3 > /dev/null
+then
+    cp "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.py "$GASH_HOME/Chateau/Observatoire/merlin"
+    chmod 755 "$GASH_HOME/Chateau/Observatoire/merlin"
+fi
+
 
