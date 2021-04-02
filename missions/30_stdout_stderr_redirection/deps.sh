2aaa278a8b790bd8d8de9b2fc6a1388897664efa
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-02 13:16:37 +0200

    utilisation de clang / gcc / cc / python3 pour la mission 30

--- missions/30_stdout_stderr_redirection/deps.sh
+++ missions/30_stdout_stderr_redirection/deps.sh
@@ -1,6 +1,6 @@
-if ! command -v gcc > /dev/null && ! command -v python3 > /dev/null ; then
-    echo "Attention, ni la commande python3 ni la commande ne sont disponibles."
+if ! command -v gcc > /dev/null ! command -v clang && ! command -v cc && ! command -v python3 > /dev/null ; then
+    echo "Attention, je n'ai trouvé ni Python3 ni un compilateur C."
     echo "La mission $_MISSION_NB ne sera pas faite"
-    echo "sous Debian / Ubuntu, il faut installer le paquet python3 et/ou gcc"
+    echo "sous Debian / Ubuntu, il faut installer un des paquets python3, gcc, ou clang"
     exit 1
 fi
--- missions/30_stdout_stderr_redirection/init.sh
+++ missions/30_stdout_stderr_redirection/init.sh
@@ -7,6 +7,12 @@ mkdir -p "$GASH_HOME/Chateau/Observatoire"
 if command -v gcc > /dev/null
 then
     gcc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
+elif command -v clang > /dev/null
+then
+    clang -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
+elif command -v cc > /dev/null
+then
+    cc -o "$GASH_HOME/Chateau/Observatoire/merlin" "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.c
 elif command -v python3 > /dev/null
 then
     cp "$GASH_MISSIONS"/*_stdout_stderr_redirection/merlin.py "$GASH_HOME/Chateau/Observatoire/merlin"
