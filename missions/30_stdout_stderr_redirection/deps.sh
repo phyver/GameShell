76c1503e6f5617b90b4b6e5471ce9704af80903e
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-02 16:52:06 +0200

    bug dans les dépendances de la mission 30

--- missions/30_stdout_stderr_redirection/deps.sh
+++ missions/30_stdout_stderr_redirection/deps.sh
@@ -1,4 +1,4 @@
-if ! command -v gcc > /dev/null ! command -v clang && ! command -v cc && ! command -v python3 > /dev/null ; then
+if ! command -v gcc > /dev/null && ! command -v clang && ! command -v cc && ! command -v python3 > /dev/null ; then
     echo "Attention, je n'ai trouvé ni Python3 ni un compilateur C."
     echo "La mission $_MISSION_NB ne sera pas faite"
     echo "sous Debian / Ubuntu, il faut installer un des paquets python3, gcc, ou clang"
