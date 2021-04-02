8c145a4c4646a2fa016ab4ca7475f9b81103f462
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-02 17:07:51 +0200

    pb de dépendance pour la mission 17

--- missions/17_bg/deps.sh
+++ missions/17_bg/deps.sh
@@ -1,11 +1,10 @@
 if ! command -v xeyes > /dev/null; then
-    echo "Attention, la commande xeyes n'est pas installée, la mission $_MISSION_NB ne sera pas faite"
+    echo "Attention, la commande xeyes n'est pas installée, la mission $_MISSION_NB ne sera pas faite."
     echo "sous Debian / Ubuntu, il faut installer le paquet x11-apps"
     exit 1
 fi
 
 if [ -z "$DISPLAY" ]; then
-    echo "Attention, la variable \$DISPLAY n'est pas définie."
-    echo "La commande xeyes ne pourra probablement pas être lancée."
+    echo "Attention, la variable \$DISPLAY n'est pas définie, la mission $_MISSION_NB ne sera pas faite."
     exit 1
 fi
