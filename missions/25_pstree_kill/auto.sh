dbe31c5a5962591159c55de36bf57522a043c076
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-04 12:56:46 +0200

    fichiers auto.sh manquants

--- missions/25_pstree_kill/auto.sh
+++ missions/25_pstree_kill/auto.sh
@@ -1,5 +1,5 @@
-p=$(ps | grep "gros_minet.sh$" | cut -f1 -d" ")
-ps -o pid,comm,ppid | grep "$p$" | grep "chat" | cut -f1 -d" " | xargs kill
+p=$(ps | grep "gros_minet.sh$" | sed 's/^ *//' | cut -f1 -d" ")
+ps -o pid,comm,ppid | grep "$p$" | grep "chat" | sed 's/^ *//' | cut -f1 -d" " | xargs kill
 rm ~/Chateau/Cave/.*_chat
 unset p
 gash check
