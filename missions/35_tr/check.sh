c2f4bf5ca05c0ebd55f5582854909711cad2ac76
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-04 12:53:36 +0200

    utilisation d'un fichier au lieu d'une variables pour la mission 35

--- missions/35_tr/check.sh
+++ missions/35_tr/check.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-if [ -z "$_SECRET_KEY" ]
-then
-    echo "Oups, probleme d'initialisation."
-    echo "La mision est relancee..."
-    false
-    return
-fi
-
 echo "Quelle est la clé qui fait apparaitre le coffre de Merlin ? "
 read -er dcode
 
-if [ "$dcode" = "$_SECRET_KEY" ]
+if [ "$dcode" = "$(cat "$GASH_TMP/secret_key")" ]
 then
-    unset dcode _SECRET_KEY
+    unset dcode
     mkdir -p "$GASH_HOME/Chateau/Cave/Coffre_de_merlin"                       # je met le coffre
     cp "$MISSION_DIR"/recette_secrete "$GASH_HOME/Chateau/Cave/Coffre_de_merlin" # et son contenu (cela ne sert à rien pour la mission, mais si 
     true                                                                    # l'utilisateur à la curiosité d'aller voir, il y aura qqch)
 else
     echo "Ce n'est pas la bonne clé.."
-    unset dcode _SECRET_KEY
+    unset dcode
     false
 fi
 
