dae094d3f1cf15c1a6bb4f8ff182202268695c6a
Victor Dubois <victor.dubois@navya.tech>
2021-04-07 12:50:26 +0200

    working translated (i18n) mission 10

--- missions/10_cp_2/init.sh
+++ missions/10_cp_2/init.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
+IN_ENTRANCE=$(CANONICAL_PATH "$(eval_gettext "\$GASH_TMP/in_entrance")")
+CASTLE_ENTRANCE=$(CANONICAL_PATH "$(eval_gettext "\$GASH_HOME/Castle/Entrance")")
+GASH_HUT=$(CANONICAL_PATH "$(eval_gettext "\$GASH_HOME/Forest/Hut")")
+HAY=$(gettext "hay")
+GRAVEL=$(gettext "gravel")
+DETRITUS=$(gettext "detritus")
+ORNEMENT=$(gettext "ornement")
+
 D=$(date +%s)
 
 for i in $(seq -w 5)
 do
     S=$(checksum "detritus_$i#$D")
-    touch "$GASH_HOME/Chateau/Entree/${S}_detritus"
+    touch "${CASTLE_ENTRANCE}/${S}_${DETRITUS}"
 done
 
 for i in $(seq -w 5)
 do
-    S=$(checksum "foin_$i#$D")
-    touch "$GASH_HOME/Chateau/Entree/${S}_foin"
+    S=$(checksum "${HAY}_$i#$D")
+    touch "${CASTLE_ENTRANCE}/${S}_${HAY}"
 done
 
 for i in $(seq -w 5)
 do
-    S=$(checksum "gravas_$i#$D")
-    touch "$GASH_HOME/Chateau/Entree/${S}_gravas"
+    S=$(checksum "${GRAVEL}_$i#$D")
+    touch "${CASTLE_ENTRANCE}/${S}_${GRAVEL}"
 done
 
 for i in $(seq -w 10)
 do
-    S=$(checksum "ornement_$i#$D")
-    touch "$GASH_HOME/Chateau/Entree/${S}_ornement"
+    S=$(checksum "${ORNEMENT}_$i#$D")
+    touch "${CASTLE_ENTRANCE}/${S}_${ORNEMENT}"
 done
 
-command ls "$GASH_HOME/Chateau/Entree" | sort > "$GASH_TMP/dans_entree"
+command ls "$CASTLE_ENTRANCE" | sort > "$IN_ENTRANCE"
 
 unset D i S cabane
