1f16b934df02b6bf9f14bfdacaee8abd8133c9ad
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-15 11:04:58 +0200

    small fixes for missions 31 and 32

--- missions/32_find_3_xargs/auto.sh
+++ missions/32_find_3_xargs/auto.sh
@@ -1,4 +1,4 @@
-mkdir -p "$GASH_COFFRE"
+mkdir -p "$GASH_CHEST"
 cd "$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -name "$(gettext "maze")" -type d)"
-find . -type f -print0 | xargs -0 grep -Zl "$(gettext "diamond")" |  xargs -0 mv -t "$GASH_COFFRE"
+find . -type f -print0 | xargs -0 grep -Zl "$(gettext "diamond")" |  xargs -0 mv -t "$GASH_CHEST"
 gash check
