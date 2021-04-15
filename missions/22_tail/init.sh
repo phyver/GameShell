b3510b4c8488b71888cdba79241100cae01f410d
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-15 16:03:59 +0200

    translating mission 22

--- missions/22_tail/init.sh
+++ missions/22_tail/init.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-echo '1/ trois mesures de sisymbre cueillies une nuit de pleine lune
-2/ deux touffes de polygonum
-3/ quatre sangsues
-4/ trois cuillères de chrysopes cuites pendant 21 jours
-5/ trois mesures de peau de serpent du Cap
-6/ une mesure de corne de bicorne
-7/ un peu de la personne en qui vous voulez vous transformer' > "$GASH_HOME/Montagne/Grotte/ingredients_potion"
-
-
+cp "$(eval_gettext '$MISSION_DIR/ingredients/en.txt')" "$(eval_gettext '$GASH_HOME/Montain/Cavern')/$(gettext "potion_ingredients")" 
