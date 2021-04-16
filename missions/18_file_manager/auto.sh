c8b6872b3e9f2bedd968f1b4c5684cdc8834e00c
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-16 21:55:42 +0200

    translating mission 18

--- missions/18_file_manager/auto.sh
+++ missions/18_file_manager/auto.sh
@@ -1,3 +1,9 @@
-cd "$(find ~/Chateau/Cave -name "labyrinthe" -type d)"
-find . -name "*piece*" -type f -print0 | xargs -0 mv -t ~/Foret/Cabane/Coffre
+#!/bin/bash
+
+mkdir -p "$GASH_CHEST"
+
+cd "$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -name "$(gettext "maze")" -type d)"
+
+find . -name "$(gettext "copper_coin")" -type f -print0 | xargs -0 mv -t "$GASH_CHEST"
+
 gash check
--- missions/18_file_manager/clean.sh
+++ missions/18_file_manager/clean.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
-
 # mise à jour du répertoire courant, qui pourrait être supprimé lors du ménage
+
 case $PWD in
-    *labyrinthe*)
-        cd "$(find "$GASH_HOME/Chateau/Cave/" -type d -name '*Long*Couloir*')" &&
-            echo "Vous voila de retour par téléporation à l'entrée du labyrinthe..."
+    *"$(gettext "maze")"*)
+        cd "$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Long*Corridor*")")" &&
+            echo "$(gettext "Pffft... You are back to the entrance of the maze...")"
         ;;
 esac
 
-rm -f "$GASH_TMP/bronze"
-find "$GASH_HOME" -iname bronze -not -iname "*journal*" -print0 | xargs -0 rm -f
-find "$GASH_HOME/Chateau/Cave/" -name labyrinthe -type d -print0 | xargs -0 rm -rf
+rm -f "$GASH_TMP/copper_coin"
+
+find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -name "$(gettext "maze")" -type d -print0 | xargs -0 rm -rf
+
+#fixme
+find "$GASH_HOME" -iname "*$(gettext "copper_coin")*" -not -iname "*journal*" -print0 | xargs -0 rm -f
