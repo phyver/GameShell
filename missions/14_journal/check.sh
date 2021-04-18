11f808f28131dd4948024f0f4d1ae127f650c92e
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-18 15:36:47 +0200

    small fixes

--- missions/14_journal/check.sh
+++ missions/14_journal/check.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-local journal="$GASH_CHEST/$(gettext "journal").txt"
-if [ ! -f "$journal" ]
+_local_check() {
+    local journal="$GASH_CHEST/$(gettext "journal").txt"
+    if [ ! -f "$journal" ]
+    then
+        echo "$(eval_gettext "The file '\$journal' doesn't exist...")"
+        find "$GASH_HOME" -iname "*$(gettext "journal")*" -print0 | xargs -0 rm -f
+        return 1
+    elif [ ! -s "$journal" ]
+    then
+        echo "$(eval_gettext "The file '\$journal' is empty...")"
+        find "$GASH_HOME" -iname "*$(gettext "journal")*" -print0 | xargs -0 rm -f
+        return 1
+    else
+        return 0
+    fi
+}
+
+if _local_check
 then
-    echo "$(eval_gettext "The file '\$journal' doesn't exist...")"
-    find "$GASH_HOME" -iname "*$(gettext "journal")*" -print0 | xargs -0 rm -f
-    unset journal
-    false
-elif [ ! -s "$journal" ]
-then
-    echo "$(eval_gettext "The file '\$journal' is empty...")"
-    find "$GASH_HOME" -iname "*$(gettext "journal")*" -print0 | xargs -0 rm -f
-    unset journal
-    false
+    unset -f _local_check
+    true
 else
-    unset journal
-    return 0
+    unset -f _local_check
+    false
 fi
+
