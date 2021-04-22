eb658f79dbfce3860ca22c54b698bfecea5cf3f9
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-04-22 23:50:11 +0200

    Fix typo in missions (Montain → Mountain).

--- missions/22_tail/auto.sh
+++ missions/22_tail/auto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$(eval_gettext '$GASH_HOME/Montain/Cave')"
+cd "$(eval_gettext '$GASH_HOME/Mountain/Cave')"
 
 # les commandes ne sont pas dans l'historique, il faut les y ajouter à la main !
 history -s tail -n 4  $(gettext "potion_ingredients")
--- missions/23_head_tail/init.sh
+++ missions/23_head_tail/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp "$(eval_gettext '$MISSION_DIR/recipe/en.txt')" "$(eval_gettext '$GASH_HOME/Montain/Cave')/$(gettext "potion_recipe")"
+cp "$(eval_gettext '$MISSION_DIR/recipe/en.txt')" "$(eval_gettext '$GASH_HOME/Mountain/Cave')/$(gettext "potion_recipe")"
 
 
