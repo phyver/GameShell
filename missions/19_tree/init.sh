1fa16f7b6825276cef8221560d35dba659247788
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-27 12:31:39 +0200

    make sure GASH_CHEST is defined and exists for the missions that require it

--- missions/19_tree/init.sh
+++ missions/19_tree/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ -z "$GASH_CHEST" ] && GASH_CHEST="$(eval_gettext '$GASH_HOME/Forest/Cabin/Chest')"
+mkdir -p "$GASH_CHEST"
+
 corridor="$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Long*Corridor*")")"
 if [ -z "$corridor" ]
 then
@@ -17,29 +20,37 @@ N=2
 r1="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
 
 echo -n "$(gettext "maze generation:")"
-for i in $(seq $N)
-do
-    I=$(checksum "$t$i")
-    mkdir -p "$lab/$I"
-    for j in $(seq $N)
+if ! command -v python3 > /dev/null
+then
+    for i in $(seq $N)
     do
-        J=$(checksum "$t$i$j")
-        mkdir -p "$lab/$I/$J"
-        for k in $(seq $N)
+        I=$(checksum "$t$i")
+        mkdir -p "$lab/$I"
+        for j in $(seq $N)
         do
-            K=$(checksum "$t$i$j$k")
-            mkdir -p "$lab/$I/$J/$K"
+            J=$(checksum "$t$i$j")
+            mkdir -p "$lab/$I/$J"
+            for k in $(seq $N)
+            do
+                K=$(checksum "$t$i$j$k")
+                mkdir -p "$lab/$I/$J/$K"
 
-            if [ "$r1" = "$i,$j,$k" ]
-            then
-                echo "$I $J $K" > "$lab/$I/$J/$K/$(gettext "silver_coin")"
-                echo "$I $J $K" > "$GASH_MISSION_DATA/silver_coin"
-            fi
+                if [ "$r1" = "$i,$j,$k" ]
+                then
+                    echo "$I $J $K" > "$lab/$I/$J/$K/$(gettext "silver_coin")"
+                    echo "$I $J $K" > "$GASH_MISSION_DATA/silver_coin"
+                fi
+            done
+            echo -n "."
         done
-        echo -n "."
     done
-done
-echo
+    echo
+else
+    mkdir -p "$lab"
+    d=$("$GASH_LOCAL_BIN"/maze_generator.py "$lab" 3 3 1)
+    echo "$(checksum "$d")" > "$lab/$d/$(gettext "silver_coin")"
+    echo "$(checksum "$d")" > "$GASH_MISSION_DATA/silver_coin"
+fi
 
 unset i j k t corridor lab N r1 r2 I J K
 
--- missions/20_find_1/init.sh
+++ missions/20_find_1/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ -z "$GASH_CHEST" ] && GASH_CHEST="$(eval_gettext '$GASH_HOME/Forest/Cabin/Chest')"
+mkdir -p "$GASH_CHEST"
+
 corridor="$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Long*Corridor*")")"
 if [ -z "$corridor" ]
 then
@@ -11,41 +14,52 @@ fi
 
 lab="$corridor/$(gettext "maze")"
 
-t=$(date +%s)
-
-N=10
-r1="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
-r2="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
-
 echo -n "$(gettext "maze generation:")"
-for i in $(seq $N)
-do
-    I=$(checksum "$t$i")
-    mkdir -p "$lab/$I"
-    for j in $(seq $N)
+if ! command -v python3 > /dev/null
+then
+    t=$(date +%s)
+    N=10
+    r1="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
+    r2="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
+
+    for i in $(seq $N)
     do
-        J=$(checksum "$t$i$j")
-        mkdir -p "$lab/$I/$J"
-        for k in $(seq $N)
+        I=$(checksum "$t$i")
+        mkdir -p "$lab/$I"
+        for j in $(seq $N)
         do
-            K=$(checksum "$t$i$j$k")
-            mkdir -p "$lab/$I/$J/$K"
+            J=$(checksum "$t$i$j")
+            mkdir -p "$lab/$I/$J"
+            for k in $(seq $N)
+            do
+                K=$(checksum "$t$i$j$k")
+                mkdir -p "$lab/$I/$J/$K"
 
-            if [ "$r1" = "$i,$j,$k" ]
-            then
-                echo "$I $J $K" > "$lab/$I/$J/$K/$(gettext "gold_coin")"
-                echo "$I $J $K" > "$GASH_MISSION_DATA/$(gettext "gold_coin")"
-            elif [ "$r2" = "$i,$j,$k" ]
-            then
-                echo "$I $J $K" > "$lab/$I/$J/$K/$(gettext "GolD_CoiN")"
-                echo "$I $J $K" > "$GASH_MISSION_DATA/$(gettext "GolD_CoiN")"
-            fi
+                if [ "$r1" = "$i,$j,$k" ]
+                then
+                    echo "$I $J $K" > "$lab/$I/$J/$K/$(gettext "gold_coin")"
+                    echo "$I $J $K" > "$GASH_MISSION_DATA/$(gettext "gold_coin")"
+                elif [ "$r2" = "$i,$j,$k" ]
+                then
+                    echo "$I $J $K" > "$lab/$I/$J/$K/$(gettext "GolD_CoiN")"
+                    echo "$I $J $K" > "$GASH_MISSION_DATA/$(gettext "GolD_CoiN")"
+                fi
+            done
+            echo -n "."
         done
-        echo -n "."
     done
-done
-echo
+    echo
+else
+    mkdir -p "$lab"
+    d=$("$GASH_LOCAL_BIN"/maze_generator.py "$lab" 3 10 2)
+    d1=$(echo "$d" | head -n 1)
+    d2=$(echo "$d" | head -n 1)
+    echo "$(checksum "$d1")" > "$lab/$d1/$(gettext "gold_coin")"
+    echo "$(checksum "$d1")" > "$GASH_MISSION_DATA/gold_coin"
+    echo "$(checksum "$d2")" > "$lab/$d2/$(gettext "GolD_CoiN")"
+    echo "$(checksum "$d2")" > "$GASH_MISSION_DATA/GolD_CoiN"
+fi
 
-unset i j k t corridor lab N r1 r2 I J K
+unset i j k t corridor lab N r1 r2 I J K d d1 d2
 
 
