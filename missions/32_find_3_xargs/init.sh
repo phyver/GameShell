42babaa992281530360858748e41189ba4159a4d
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-27 12:31:39 +0200

    make sure GASH_CHEST is defined and exists for the missions that require it

--- missions/32_find_3_xargs/init.sh
+++ missions/32_find_3_xargs/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ -z "$GASH_CHEST" ] && GASH_CHEST="$(eval_gettext '$GASH_HOME/Forest/Cabin/Chest')"
+mkdir -p "$GASH_CHEST"
+
 corridor="$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Long*Corridor*")")"
 if [ -z "$corridor" ]
 then
