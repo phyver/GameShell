a5a9a571de1887b95f6c8eb3931d731985e15b85
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-28 16:13:07 +0200

    cleaning mission 27

--- missions/27_stdin_redirection/static.sh
+++ missions/27_stdin_redirection/static.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-
 mkdir -p "$(eval_gettext '$GASH_HOME/Castle/Main_building/Library')"
-
 touch "$(eval_gettext '$GASH_HOME/Castle/Main_building/Library')/$(gettext "Mathematics_101")"
-
 touch "$(eval_gettext '$GASH_HOME/Castle/Main_building/Library')/$(gettext "Greek_Latin_and_other_modern_languages")"
