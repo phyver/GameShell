becdc84d83d22d332889cdc3552b635c5e7ffaf9
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-28 16:16:42 +0200

    cleaning mission 30

--- missions/30_stdout_stderr_redirection/deps.sh
+++ missions/30_stdout_stderr_redirection/deps.sh
@@ -1,6 +1,5 @@
 if ! command -v gcc > /dev/null && ! command -v clang && ! command -v cc && ! command -v python3 > /dev/null ; then
-    echo "Attention, je n'ai trouvé ni Python3 ni un compilateur C."
-    echo "La mission $_MISSION_NB ne sera pas faite"
-    echo "sous Debian / Ubuntu, il faut installer un des paquets python3, gcc, ou clang"
-    exit 1
+    echo "$(eval_gettext "The python3 interpreter, or a C compiler is required for mission \$MISSION_NAME.
+(Debian / Ubuntu: install one of the packages 'python3', 'gcc' or 'clang')")"
+    false
 fi
