497cebe7828d129ba124f662a36ce45701973fb2
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-28 15:09:45 +0200

    cleaning and test.sh for mission 32

--- missions/32_find_3_xargs/init.sh
+++ missions/32_find_3_xargs/init.sh
@@ -3,57 +3,59 @@
 [ -z "$GASH_CHEST" ] && GASH_CHEST="$(eval_gettext '$GASH_HOME/Forest/Cabin/Chest')"
 mkdir -p "$GASH_CHEST"
 
-corridor="$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Long*Corridor*")")"
-if [ -z "$corridor" ]
-then
-    r1=$(checksum $RANDOM)
-    r2=$(checksum $RANDOM)
-    corridor="$(eval_gettext '$GASH_HOME/Castle/Cellar')/$(eval_gettext '.Long $r1 Corridor $r2')"
-    mkdir -p "$corridor"
-fi
+maze="$(eval_gettext '$GASH_HOME/Botanical_garden/.Maze')"
+rm -rf "$maze"/?*
 
-lab="$corridor/$(gettext "maze")"
-
-if ! command -v python3 > /dev/null
-then
+gen_maze_sh(){
     echo -n "$(gettext "maze generation:")"
-    t=$(date +%s)
+    local t=$(date +%s)
 
-    N=10
-    r1="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
+    local N=10
+    local r1="$((1 + RANDOM%N)),$((1 + RANDOM%N)),$((1 + RANDOM%N))"
 
+    local i I j J k K
     for i in $(seq $N)
     do
         I=$(checksum "$t$i")
-        mkdir -p "$lab/$I"
+        mkdir -p "$maze/$I"
         for j in $(seq $N)
         do
             J=$(checksum "$t$i$j")
-            mkdir -p "$lab/$I/$J"
+            mkdir -p "$maze/$I/$J"
             for k in $(seq $N)
             do
                 K="$t$i$j$k"
                 if [ "$r1" = "$i,$j,$k" ]
                 then
-                    sum=$(checksum "$K $(gettext "diamond")")
-                    echo "$K $(gettext "diamond") $sum" > "$lab/$I/$J/$K"
+                    local sum=$(checksum "$K $(gettext "diamond")")
+                    echo "$K $(gettext "diamond") $sum" > "$maze/$I/$J/$K"
                     echo "$K $(gettext "diamond") $sum" > "$GASH_MISSION_DATA/diamond"
                 else
-                    sum=$(checksum "$K $(gettext "stone")")
-                    echo "$K $(gettext "stone") $K $sum" > "$lab/$I/$J/$K"
+                    local sum=$(checksum "$K $(gettext "stone")")
+                    echo "$K $(gettext "stone") $K $sum" > "$maze/$I/$J/$K"
                 fi
             done
             echo -n "."
         done
     done
     echo
-else
-    mkdir -p "$lab"
-    f=$(python3 "$MISSION_DIR"/init.py "$lab" 3 10 1)
-    K=$(basename "$f")
-    sum=$(checksum "$K $(gettext "diamond")")
-    echo "$K $(gettext "diamond") $sum" > "$lab/$f"
+}
+
+gen_maze_py(){
+    mkdir -p "$maze"
+    local f=$(python3 "$MISSION_DIR"/init.py "$maze" 3 10 1)
+    local K=$(basename "$f")
+    local sum=$(checksum "$K $(gettext "diamond")")
+    echo "$K $(gettext "diamond") $sum" > "$maze/$f"
     echo "$K $(gettext "diamond") $sum" > "$GASH_MISSION_DATA/diamond"
+}
+
+if ! command -v python3 > /dev/null
+then
+    gen_maze_sh
+else
+    gen_maze_py
 fi
 
-unset i j k t corridor lab N r1 r2 I J K f sum
+unset maze
+unset -f gen_maze_sh gen_maze_py
