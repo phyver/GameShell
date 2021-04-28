72c8ef42eba8a37d90f28bc404251de6326d2e7a
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-28 15:59:35 +0200

    cleaning and test.sh for mission 23

--- missions/23_head_tail/auto.sh
+++ missions/23_head_tail/auto.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
-
 cd "$(eval_gettext '$GASH_HOME/Mountain/Cave')"
 
-# les commandes ne sont pas dans l'historique, il faut les y ajouter à la main !
+# commands from a sourced file aren't saved in the history,
+# we need to do that explicitly
 history -s "head -n 11 \"$(gettext "potion_recipe")\" | tail -n 3"
 history -s gash check
-
-head -n 11 "$(gettext "potion_recipe")" | tail -n 3
 gash check
 
 history -d -2--1
-
