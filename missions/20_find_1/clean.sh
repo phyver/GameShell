b879b60ad197c840ac26ea66549d93e504a740f8
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-28 14:31:50 +0200

    cleaning and test.sh for mission 20

--- missions/20_find_1/clean.sh
+++ missions/20_find_1/clean.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-
-
-# mise à jour du répertoire courant, qui pourrait être supprimé lors du ménage
+maze="$(eval_gettext '$GASH_HOME/Botanical_garden/.Maze')"
 case $PWD in
-    *"$(gettext "maze")"*)
-        cd "$(find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Long*Corridor*")")" &&
-            echo "$(gettext "Pffft... You are back to the entrance of the maze...")"
+    "$maze"/?*)
+        cd "$maze"
+        echo "$(gettext "Pffft... You are back to the entrance of the maze...")"
         ;;
 esac
 
-find "$(eval_gettext '$GASH_HOME/Castle/Cellar')" -name "$(gettext "maze")" -type d -print0 | xargs -0 rm -rf
-# FIXME
-rm -f "$GASH_MISSION_DATA/$(gettext "gold_coin")" "$GASH_MISSION_DATA/$(gettext "GolD_CoiN")"
+rm -f "$GASH_MISSION_DATA/{gold_coin,GolD_CoiN}"
+rm -rf "$maze"/*
