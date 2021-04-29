72c0a7e145d298e568e202e7753564868fd3b8f3
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-29 21:04:17 +0200

    fix for mission 24

--- missions/24_ps_kill/check.sh
+++ missions/24_ps_kill/check.sh
@@ -10,11 +10,17 @@ _local_check() {
 
     local dir=$(eval_gettext '$GASH_HOME/Castle/Kitchen')/$(gettext "Pantry")
     local cat=$(find "$dir" -name "*_$(gettext "wind-up_cat")")
-    local _cat=$(find "$dir" -name ".*_$(gettext "wind-up_cat")")
 
-    if [ -n "$cat" ] || [ -n "$_cats" ]
+    if [ -n "$cat" ]
     then
         echo "$(gettext "There still are some wind-up cats in the pantry!")"
+        return 1
+    fi
+
+    cat=$(find "$dir" -name ".*_$(gettext "wind-up_cat")")
+    if [ -n "$cat" ]
+    then
+        echo "$(gettext "There still are some hidden wind-up cats in the pantry!")"
         return 1
     fi
 
