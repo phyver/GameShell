c9c6cce5d6431d7046bb8c771dbba591ee5664c8
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-29 15:10:57 +0200

    added the king's quarter

--- missions/03_cd_3/static.sh
+++ missions/03_cd_3/static.sh
@@ -1,1 +1,11 @@
-mkdir -p "$(eval_gettext "\$GASH_HOME/Castle/Main_building/Throne_room")"
+dir="$(eval_gettext '$GASH_HOME/Castle/Main_building/Throne_room')"
+mkdir -p "$dir"
+mkdir -p "$dir/$(gettext "Kings_quarter")"
+cat <<EOK>"$dir/$(gettext "Kings_quarter")/$(gettext "crown")"
+  _.+._
+(^\/^\/^)
+ \@*@*@/
+ {_____}
+EOK
+chmod 000 "$dir/$(gettext "Kings_quarter")"
+unset dir
