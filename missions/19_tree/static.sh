857d3c7afed14141301759748a26cce54f490908
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-29 14:56:49 +0200

    added a shed and an orchard in the garden

--- missions/19_tree/static.sh
+++ missions/19_tree/static.sh
@@ -1,1 +1,3 @@
 mkdir -p "$(eval_gettext '$GASH_HOME/Botanical_garden/.Maze')"
+mkdir -p "$(eval_gettext '$GASH_HOME/Botanical_garden/Orchard')"
+mkdir -p "$(eval_gettext '$GASH_HOME/Botanical_garden/Shed')"
