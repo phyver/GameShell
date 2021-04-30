e289144b4d06f8100f511e08e4cf835251031401
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-30 16:14:31 +0200

    fix for mission 3: removed crown

--- missions/03_cd_3/static.sh
+++ missions/03_cd_3/static.sh
@@ -1,11 +1,1 @@
-dir="$(eval_gettext '$GASH_HOME/Castle/Main_building/Throne_room')"
-mkdir -p "$dir"
-mkdir -p "$dir/$(gettext "Kings_quarter")"
-cat <<EOK>"$dir/$(gettext "Kings_quarter")/$(gettext "crown")"
-  _.+._
-(^\/^\/^)
- \@*@*@/
- {_____}
-EOK
-chmod -R -w "$dir/$(gettext "Kings_quarter")"
-unset dir
+mkdir -p "$(eval_gettext '$GASH_HOME/Castle/Main_building/Throne_room')"
