4739c9a76d25420114c01cfe339ecd10f02f41ad
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-04-30 11:05:24 +0200

    translation of deps.sh in mission 25

--- missions/25_pstree_kill/deps.sh
+++ missions/25_pstree_kill/deps.sh
@@ -1,5 +1,5 @@
 if ! command -v pstree > /dev/null; then
-    echo "Attention, la commande pstree n'est pas installée, la mission $_MISSION_NB ne sera pas faite"
-    echo "sous Debian / Ubuntu, il faut installer le paquet psmisc"
-    exit 1
+    echo "$(eval_gettext "The command 'pstree' is required for mission \$MISSION_NAME.
+(Debian / Ubuntu: install package 'psmisc')")"
+    false
 fi
