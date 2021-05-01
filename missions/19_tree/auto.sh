e08cd87235ce47c92d62e948ea97f2d08b82bae2
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-01 22:42:44 +0200

    fix mission 19: botanical garden => garden

--- missions/19_tree/auto.sh
+++ missions/19_tree/auto.sh
@@ -1,2 +1,2 @@
-find "$(eval_gettext '$GASH_HOME/Botanical_garden/.Maze')" -name "*$(gettext "silver_coin")*" -type f -print0 | xargs -0 mv -t $GASH_CHEST
+find "$(eval_gettext '$GASH_HOME/Garden/.Maze')" -name "*$(gettext "silver_coin")*" -type f -print0 | xargs -0 mv -t $GASH_CHEST
 gash check
