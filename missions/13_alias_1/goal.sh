8c9e174fceba5a47eff67cb0e39bbe643aab146a
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-01 22:05:58 +0200

    fix mission 13, added a hidden file
    gash show adds it at the players location to check the alias

---
+++ missions/13_alias_1/goal.sh
@@ -0,0 +1,10 @@
+cat <<EOF > ./".$(gettext "nice_rock")"
+  .     '     ,
+    _________
+ _ /_|_____|_\ _
+   '. \   / .'
+     '.\ /.'
+       '.'
+EOF
+cat "$(eval_gettext '$MISSION_DIR/goal/en.txt')"
+
