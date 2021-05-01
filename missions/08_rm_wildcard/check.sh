7d061c3415091c6604aaec38b6566d8351ae10d0
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-01 21:45:21 +0200

    fix mission 8
    and a remark about shell patterns and hidden files

--- missions/08_rm_wildcard/check.sh
+++ missions/08_rm_wildcard/check.sh
@@ -6,7 +6,7 @@ _local_check() {
     local nb_spiders=$(find "$CELLAR" -maxdepth 1 -name "*$(gettext "spider")" | wc -l)
     if [ "$nb_spiders" -ne 0 ]
     then
-        echo "$(eval_gettext "There still are some spiders ($nb_spiders) in the cellar!")"
+        echo "$(eval_gettext "There still are some spiders (\$nb_spiders) in the cellar!")"
         return 1
     fi
 
