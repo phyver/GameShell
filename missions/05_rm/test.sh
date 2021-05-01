0bba6ceec9fed44e7f609dc5e29c8c946d0cc8d2
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-01 21:30:42 +0200

    added second salamander to mission 5

--- missions/05_rm/test.sh
+++ missions/05_rm/test.sh
@@ -6,3 +6,6 @@ gash assert check false
 rm "$(eval_gettext '$GASH_HOME/Castle/Cellar')/$(gettext "spider")"?
 gash assert check true
 
+rm "$(eval_gettext '$GASH_HOME/Castle/Cellar')/$(gettext "spider")"?
+rm "$(eval_gettext '$GASH_HOME/Castle/Cellar')/$(gettext "salamander")"1
+gash assert check false
