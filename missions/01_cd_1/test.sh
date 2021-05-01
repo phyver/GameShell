8fe020e211e9768c66bff922f037e08204fe6e8b
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-01 22:21:00 +0200

    fix mission 1: dungeon => tower

--- missions/01_cd_1/test.sh
+++ missions/01_cd_1/test.sh
@@ -6,10 +6,10 @@ gash assert_check false
 cd
 gash assert_check false
 
-cd "$(eval_gettext "\$GASH_HOME/Castle/Dungeon/First_floor/Second_floor/Top_of_the_dungeon")"
+cd "$(eval_gettext "\$GASH_HOME/Castle/Main_tower/First_floor/Second_floor/Top_of_the_tower")"
 gash assert_check true
 
-cd "$(eval_gettext "\$GASH_HOME/Castle/Dungeon/First_floor/Second_floor/Top_of_the_dungeon")/.."
+cd "$(eval_gettext "\$GASH_HOME/Castle/Main_tower/First_floor/Second_floor/Top_of_the_tower")/.."
 gash assert_check false
 
 
