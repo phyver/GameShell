eaeb6478c1c1964d5b5daa5467ae6fb942c217c6
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-01 22:37:31 +0200

    fix mission 19: clean message

--- missions/19_tree/clean.sh
+++ missions/19_tree/clean.sh
@@ -2,7 +2,7 @@ maze="$(eval_gettext '$GASH_HOME/Botanical_garden/.Maze')"
 case $PWD in
     "$maze"/?*)
         cd "$maze"
-        echo "$(gettext "Pffft... You are back to the entrance of the maze...")"
+        echo "$(gettext "You are back at the entrance of the maze...")"
         ;;
 esac
 
