b379ff11a2ee3dcf8f497d147fae9d97df4dcdab
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-02 23:50:00 +0200

    added comment for some strange feature of fc in some check.sh

--- missions/03_cd_3/check.sh
+++ missions/03_cd_3/check.sh
@@ -3,8 +3,11 @@
 goal=$(REALPATH "$(eval_gettext "\$GASH_HOME/Castle/Main_building/Throne_room")")
 current=$(REALPATH "$PWD")
 
-# The third-to-last command in the history (xargs removes trailing spaces).
-ppc=$(fc -nl -3 -3 | xargs)
+# TODO: for some unknown reason, redirecting the output of fc into another
+# command shifts the results: it then sees the "gash check" command that
+# was used to run this function
+# Because of the previous remark, I need to look at the "-3" command.
+ppc=$(fc -nl -3 -3 | sed 's/^\s*//' | sed 's/\s*$//')
 
 # TODO also accept other commands to go back to the starting point?
 if [ "$goal" = "$current" ] && [  "$ppc" = "cd" ]
