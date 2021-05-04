36b30da46867f817b277145caf16e4c5ac218b0f
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-05-04 23:52:17 +0200

    Use dummy mission in the book of potions group.

--- missions/pipe_basics/01_head/init.sh
+++ missions/pipe_basics/01_head/init.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-BOOK="$(eval_gettext '$GASH_HOME/Mountain/Cave')/$(gettext "Book_of_Potions")"
+# Install the book for the mission.
+bash "$GASH_LOCAL_BIN/install_potion_book.sh" book "$(eval_gettext '$GASH_HOME/Mountain/Cave')/"
 
-mkdir -p "$BOOK"
-
-csplit -s --suppress-matched --prefix="$BOOK/$(gettext "page")_" \
-  "$(eval_gettext '$MISSION_DIR/book_of_potions/en.txt')" "/^==.*/" "{*}"
-
-mv "$BOOK/$(gettext "page")_00" "$BOOK/$(gettext "table_of_contents")"
-
-# Save a copy of the book to check that it was not altered later.
-rm -rf "$GASH_MISSION_DATA/book_of_potions"
-cp -r "$BOOK" "$GASH_MISSION_DATA/book_of_potions"
-
-unset BOOK
+# Install a copy for later checks. TODO: only do that in the check file?
+bash "$GASH_LOCAL_BIN/install_potion_book.sh" book "$GASH_MISSION_DATA/book_of_potions"
