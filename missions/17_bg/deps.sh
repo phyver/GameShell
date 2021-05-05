7d3b589a23690181baab910ff65ead1a6c3e14e7
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-05 15:58:20 +0200

    deps.sh for 'ps'

--- missions/17_bg/deps.sh
+++ missions/17_bg/deps.sh
@@ -2,10 +2,12 @@ if [ -z "$DISPLAY" ]; then
     echo "$(eval_gettext "The variable DISPLAY is not defined.
 A running X server is required for mission \$MISSION_NAME.")"
     false
-fi
-
-if ! command -v xeyes > /dev/null; then
+elif ! command -v xeyes > /dev/null; then
     echo "$(eval_gettext "The command 'xeyes' is required for mission \$MISSION_NAME.
 (Debian / Ubuntu: install package 'x11-apps')")"
+    false
+elif ! command -v ps > /dev/null; then
+    echo "$(eval_gettext "The command 'ps' is required for mission \$MISSION_NAME.
+(Debian / Ubuntu: install package 'psproc')")"
     false
 fi
