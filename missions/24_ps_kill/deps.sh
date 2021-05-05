11eadcec268d7274ff98fa2929d8092438e67dd8
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-05 15:58:20 +0200

    deps.sh for 'ps'

---
+++ missions/24_ps_kill/deps.sh
@@ -0,0 +1,5 @@
+if ! command -v ps > /dev/null; then
+    echo "$(eval_gettext "The command 'ps' is required for mission \$MISSION_NAME.
+(Debian / Ubuntu: install package 'psproc')")"
+    false
+fi
