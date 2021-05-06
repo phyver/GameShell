d6cbc069f3598ed49abbe5aef0046724c6ceacf8
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-05-07 00:10:16 +0200

    Fix tests for the missions with the book of potions.

--- missions/pipe_basics/02_tail/test.sh
+++ missions/pipe_basics/02_tail/test.sh
@@ -1,28 +1,28 @@
 cd "$(eval_gettext '$GASH_HOME/Mountain/Cave')"
 
-history -s tail -n 9 $(gettext "Book_of_Potions")/$(gettext "page")_12
-history -s gash check
+history -s "tail -n 9 $(gettext "Book_of_Potions")/$(gettext "page")_12"
+history -s "gash check"
 gash assert check true
 history -d -2--1
 
-history -s tail -n 9 $(gettext "Book_of_Potions")/$(gettext "page")_12
-history -s something something
-history -s gash check
+history -s "tail -n 9 $(gettext "Book_of_Potions")/$(gettext "page")_12"
+history -s "something something"
+history -s "gash check"
 gash assert check false
 history -d -3--1
 
-history -s tail -n 10 $(gettext "Book_of_Potions")/$(gettext "page")_12
-history -s gash check
+history -s "tail -n 10 $(gettext "Book_of_Potions")/$(gettext "page")_12"
+history -s "gash check"
 gash assert check false
 history -d -2--1
 
-history -s tail -n 8 $(gettext "Book_of_Potions")/$(gettext "page")_12
-history -s gash check
+history -s "tail -n 8 $(gettext "Book_of_Potions")/$(gettext "page")_12"
+history -s "gash check"
 gash assert check false
 history -d -2--1
 
 sed -i '1d' $(gettext "Book_of_Potions")/$(gettext "page")_12
-history -s tail -n 9 $(gettext "Book_of_Potions")/$(gettext "page")_12
-history -s gash check
+history -s "tail -n 9 $(gettext "Book_of_Potions")/$(gettext "page")_12"
+history -s "gash check"
 gash assert check false
 history -d -2--1
