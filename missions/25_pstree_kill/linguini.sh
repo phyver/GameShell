30e58ccf6028134d9b57ba025f91c5eaadf0828b
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-07 23:34:10 +0200

    better check for mission 25 (without sleep)

--- missions/25_pstree_kill/linguini.sh
+++ missions/25_pstree_kill/linguini.sh
@@ -7,10 +7,13 @@ nature=$(gettext "cheese")
 logfile=$GSH_VAR/cheese
 
 "$GSH_VAR"/generator 0 "$logfile-0" "$dir" "$nature" &
+echo -n "$!," > "$GSH_VAR"/cheese-generator.pid
 disown
 "$GSH_VAR"/generator 1 "$logfile-1" "$dir" "$nature" &
+echo -n "$!," >> "$GSH_VAR"/cheese-generator.pid
 disown
 "$GSH_VAR"/generator 2 "$logfile-2" "$dir" "$nature" &
+echo -n "$!" >> "$GSH_VAR"/cheese-generator.pid
 disown
 
 trap "" SIGTERM SIGINT
