38bbc528b68b958a29d5f1305f43f9af340b598d
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-07 22:55:03 +0200

    translated test.sh for mission 04, and cleaning stray dirs

--- missions/04_mkdir/test.sh
+++ missions/04_mkdir/test.sh
@@ -3,24 +3,24 @@ find "$GSH_HOME" -iname "*$(gettext "Chest")*" -print0 | xargs -0 rm -rf
 
 gsh assert check false
 
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/hut"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/hut/chest"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut" | tr 'A-Z' 'a-z')"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut" | tr 'A-Z' 'a-z')/$(gettext "Chest" | tr 'A-Z' 'a-z')"
 gsh assert check false
 
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Chest"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Chest")"
 gsh assert check false
 
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Chest"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut/Chest"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Chest")"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")/$(gettext "Chest")"
 gsh assert check false
 
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Chest"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut/Chest"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Chest")"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")/$(gettext "Chest")"
 gsh assert check false
 
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut"
-mkdir "$(eval_gettext '$GSH_HOME/Forest')/Hut/Chest"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")"
+mkdir "$(eval_gettext '$GSH_HOME/Forest')/$(gettext "Hut")/$(gettext "Chest")"
 gsh assert check true
