598a161c31b45ded258b8894c96c9525f95c5064
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-07 23:34:10 +0200

    better check for mission 25 (without sleep)

--- missions/25_pstree_kill/check.sh
+++ missions/25_pstree_kill/check.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
 _mission_check() {
-    # pour laisser le temps au générateur de faire des chats
-    echo -n .
-    sleep 1
-    echo -n .
-    sleep 1
-    echo .
+    local pid
+    pid=$(cat "$GSH_VAR"/linguini.pid)
+    if ! ps -p $pid | grep linguini > /dev/null
+    then
+        local NAME=Linguini
+        echo "$(eval_gettext "Did you kill \$NAME?")"
+        return 1
+    fi
+    pid=$(cat "$GSH_VAR"/skinner.pid)
+    if ! ps -p $pid | grep skinner > /dev/null
+    then
+        local NAME=Skinner
+        echo "$(eval_gettext "Did you kill \$NAME?")"
+        return 1
+    fi
+
+    local nb=$(ps -p $(cat "$GSH_VAR"/cheese-generator.pid) | grep generator | wc -l)
+    if [ "$nb" -lt 3 ]
+    then
+        echo "$(gettext "Did you kill some cheese generator?")"
+        return 1
+    fi
+
+    local nb=$(ps -p $(cat "$GSH_VAR"/poison-generator.pid) | grep generator | wc -l)
+    if [ "$nb" -ne 0 ]
+    then
+        echo "$(gettext "Are you sure you killed all the rat poison generators?")"
+        return 1
+    fi
 
     local kitchen=$(eval_gettext '$GSH_HOME/Castle/Kitchen')
     local poison=$(ls "$kitchen"/*_"$(gettext "rat_poison")" 2>/dev/null)
@@ -20,17 +43,6 @@ _mission_check() {
     if [ -n "$poison" ]
     then
         echo "$(gettext "There still is some rat poison hidden in the kitchen!")"
-        return 1
-    fi
-
-    nbp=$(ps -e | grep "generator" | wc -l)
-    if [ "$nbp" -gt 3 ]
-    then
-        echo "$(gettext "Are you sure you killed all the rat poison generators?")"
-        return 1
-    elif [ "$nbp" -lt 3 ]
-    then
-        echo "$(gettext "Did you kill some cheese generator?")"
         return 1
     fi
 
