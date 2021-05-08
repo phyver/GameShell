1b3df0ace8395ebcd5bcb6bd9a6613e884ced43f
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-08 13:12:57 +0200

    fix auto.sh in 04_pipe and 05_wc

--- missions/pipe_intro_book_of_potions/05_wc/auto.sh
+++ missions/pipe_intro_book_of_potions/05_wc/auto.sh
@@ -2,8 +2,8 @@ cd "$(eval_gettext '$GSH_HOME/Mountain/Cave')"
 
 # commands from a sourced file aren't saved in the history,
 # we need to do that explicitly
-history -s "tail -n +4 \"$(gettext "Book_of_Potions")/$(gettext "page")_06\" | wc - l"
-history -s "gsh check"
+history -s "tail -n +4 \"$(gettext "Book_of_Potions")/$(gettext "page")_06\" | wc -l"
+history -s gsh check
 gsh check
 
 history -d -2--1
