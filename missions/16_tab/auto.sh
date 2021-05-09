28e3199ee5e16362f6d6f76523c08e008cadbdb4
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-09 21:56:00 +0200

    better mission for tab completion

--- missions/16_tab/auto.sh
+++ missions/16_tab/auto.sh
@@ -1,2 +1,6 @@
-cd "$(find "$(eval_gettext '$GSH_HOME/Castle/Cellar')" -name "$(gettext ".Long*Corridor*")" -type d)"
+lair="$(find "$(eval_gettext '$GSH_HOME/Castle/Cellar')" -type d -name "$(gettext ".Lair_of_the_spider_queen")*")"
+cd "$lair"
+queen=$(find "$lair" -type f -name "*$(gettext "spider_queen")*")
+rm -f "$queen"
+unset lair queen
 gsh check
