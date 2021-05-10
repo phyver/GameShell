5b4b195fa64aeff7b7f374f55ee36d2aba6ce6b5
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-10 23:51:04 +0200

    allow python process for mission 02_ps_kill_signal
    so we don't have a "sleep" cluttering the display from ps

--- missions/processes/02_ps_kill_signal/init.sh
+++ missions/processes/02_ps_kill_signal/init.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-TEXTDOMAIN=$(textdomainname "$MISSION_DIR")
-sed "s/export TEXTDOMAIN=.*/export TEXTDOMAIN=$TEXTDOMAIN/" "$MISSION_DIR/spell.sh" > "$GSH_VAR/$(gettext "spell")"
+if [ -x /usr/bin/python3 ]
+then
+    cp "$MISSION_DIR/spell.py" "$GSH_VAR/$(gettext "spell")"
+else
+    cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
+fi
 chmod 755 "$GSH_VAR/$(gettext "spell")"
-"$GSH_VAR/$(gettext "spell")" &
+"$GSH_VAR/$(gettext "spell")" "$TEXTDOMAIN" &
 echo $! > "$GSH_VAR/spell.pids"
 disown
