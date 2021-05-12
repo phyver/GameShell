effdb87c08813225335a920aed80d3c4c918d473
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-05-12 23:04:17 +0200

    Better progress bar approach, used in a mission.

--- missions/stdin_stdout_stderr/04_stderr_dev-null_grimoires/init.sh
+++ missions/stdin_stdout_stderr/04_stderr_dev-null_grimoires/init.sh
@@ -4,23 +4,28 @@ bib="$(eval_gettext '$GSH_HOME/Castle/Main_building/Library/Merlin_s_office')"
 find "$bib" -type f -name "$(gettext "grimoire")_*" -print0 | xargs -0 rm -f
 
 rm -f "$GSH_VAR/list_grimoires_RO"
-for i in $(seq 100)
-do
+
+_init_grimoires () {
+  for i in $(seq 100)
+  do
     file="$bib/$(gettext "grimoire")_$(checksum $RANDOM)"
     tr -dc A-Za-z </dev/urandom | head -c 100 > "$file"
-
+  
     if [ $(( RANDOM % 2 )) -eq 0 ]
     then
-        chmod -r "$file"
-        echo "$file" >> "$GSH_VAR/list_grimoires_RO"
+      chmod -r "$file"
+      echo "$file" >> "$GSH_VAR/list_grimoires_RO"
     fi
-    [ $((i%5)) -eq 0 ] && echo -n "."
-done
-echo
+    [ $((i%5)) -eq 0 ] && printf "."
+  done
+  printf "\n"
+}
+
+_init_grimoires | progress_bat
 
 bash <<EOS
   cd $bib
   grep -il "pq" * 2> /dev/null | sort > $GSH_VAR/list_grimoires_PQ
 EOS
 
-unset i file
+unset -f _init_grimoires i file
