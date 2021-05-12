38f1246985ea2dc6989094e3034eefdf134f8cf5
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-05-13 00:45:34 +0200

    Add treasure for mission 15.

--- missions/intermediate/02_alias_journal/treasure.sh
+++ missions/intermediate/02_alias_journal/treasure.sh
@@ -1,1 +1,11 @@
 alias "$(gettext "journal")"="nano $GSH_CHEST/$(gettext "journal").txt"
+
+if [ ! -f "$GSH_CHEST/bashrc" ]
+then
+  cp "$(eval_gettext '$MISSION_DIR/bashrc/en.sh')" "$GSH_CHEST/bashrc"
+fi
+
+if [ -f "$GSH_CHEST/bashrc" ]
+then
+  source "$GSH_CHEST/bashrc"
+fi
