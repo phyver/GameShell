c1db9dbf4d684e3f500311e1057540809b5a5202
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-15 22:32:36 +0200

    added some ascii-art in a couple of places

--- missions/stdin_stdout_stderr/03_stdout_redirection_inventory/static.sh
+++ missions/stdin_stdout_stderr/03_stdout_redirection_inventory/static.sh
@@ -1,1 +1,2 @@
 mkdir -p "$(eval_gettext '$GSH_HOME/Castle/Main_building/Library/Merlin_s_office')/$(gettext "Drawer")"
+sign_file "$MISSION_DIR/ascii-art/candle.txt" "$(eval_gettext '$GSH_HOME/Castle/Main_building/Library/Merlin_s_office')/$(gettext "candle")"
