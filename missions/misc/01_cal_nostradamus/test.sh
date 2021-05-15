f063fea2bc37fe3b020f0a3454254330a2c39d52
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-15 20:48:23 +0200

    better "DOW" function that works in GNU linux and freebsd (mission 10_cal_nostradamus)

--- missions/misc/01_cal_nostradamus/test.sh
+++ missions/misc/01_cal_nostradamus/test.sh
@@ -1,3 +1,21 @@
+_DOW() {
+    local dow
+    if dow=$(date --date="$1" +%A 2> /dev/null)
+    then
+        # with gnu "date" command
+        echo $dow
+        return 0
+    elif dow=$(date -jf "%Y-%m-%d" "$1" +%A 2> /dev/null)
+    then
+        # with freebsd "date" command
+        echo $dow
+        return 0
+    else
+        echo "$(gettext "Error: can not get day of week with 'date' command.")" >&2
+        return 1
+    fi
+}
+
 get_answer() {
     local offset=$1
 
@@ -5,11 +23,11 @@ get_answer() {
     local MM=$(cut -d"-" -f2 "$GSH_VAR"/date)
     local DD=$(cut -d"-" -f3 "$GSH_VAR"/date)
 
-    local day=$(date --date="$YYYY-$MM-$DD" +%A)
+    local day=$(_DOW "$YYYY-$MM-$DD")
     local i
     for i in $(seq 7)
     do
-        local answer=$(date --date="2000-05-$i" +%A)
+        local answer=$(_DOW "2000-05-$i")
         if [ "$day" = "$answer" ]
         then
             echo $((1+(i + offset - 1) % 7))
@@ -34,4 +52,4 @@ gsh assert check false < <(get_answer 6) > /dev/null
 echo -n "."
 gsh assert check true  < <(get_answer 7) > /dev/null
 
-unset -f get_answer
+unset -f get_answer _DOW
