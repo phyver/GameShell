f2a82a74a7862d1a3e44c0a36d50a1eae8b4d1f9
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-16 09:24:30 +0200

    added better clean.sh script in 07_mv_hidden_coins

--- missions/basic/07_mv_hidden_coins_garden/check.sh
+++ missions/basic/07_mv_hidden_coins_garden/check.sh
@@ -2,9 +2,9 @@
 
 # Receives coin number as first argument.
 _mission_check() {
-    local COIN_NAME=".$(gettext "coin")_$1"
+    local COIN_NAME="$(gettext "coin")_$1"
 
-    local n=$(find "$(eval_gettext '$GSH_HOME/Garden')" -maxdepth 1 -name "$COIN_NAME*" | wc -l)
+    local n=$(find "$(eval_gettext '$GSH_HOME/Garden')" -maxdepth 1 -name ".*_$COIN_NAME" | wc -l)
     if [ "$n" -gt 1 ]
     then
         echo "$(eval_gettext "There are several '\$COIN_NAME...' in the garden!")"
@@ -15,7 +15,7 @@ _mission_check() {
         return 1
     fi
 
-    local n=$(find "$GSH_CHEST" -maxdepth 1 -name "$COIN_NAME*" | wc -l)
+    local n=$(find "$GSH_CHEST" -maxdepth 1 -name ".*_$COIN_NAME" | wc -l)
     if [ "$n" -gt 1 ]
     then
         echo "$(eval_gettext "There are several '\$COIN_NAME...' in your chest!")"
@@ -26,37 +26,15 @@ _mission_check() {
         return 1
     fi
 
-    local chest_coin=$(find "$GSH_CHEST" -maxdepth 1 -name "$COIN_NAME*")
-    # Verify the CHECKSUM in the coin.
-    local COIN_DATA=$(cut -f 1 -d ' ' "$chest_coin")
-    local CHECK_SUM=$(cut -f 2 -d ' ' "$chest_coin")
-    if [ "$CHECK_SUM" != "$(CHECKSUM "$COIN_DATA")" ]
+    local chest_coin=$(find "$GSH_CHEST" -maxdepth 1 -name ".*_$COIN_NAME")
+    # check the coin.
+    if ! check_file "$chest_coin"
     then
         echo "$(eval_gettext "The coin '\$COIN_NAME...' has been tampered with...")"
-        return 1
-    fi
-
-    # Verify the the CHECKSUM also match the file name.
-    if [ "$CHECK_SUM" != "$(basename "$chest_coin" | cut -f 3 -d '_')" ]
-    then
-        echo "$(eval_gettext "The coin '\$COIN_NAME...' has a wrong file name...")"
         return 1
     fi
 
     return 0
 }
 
-# FIXME
-if _mission_check 1 && _mission_check 2 && _mission_check 3
-then
-    unset -f _mission_check
-    unset GSH_CHEST
-    true
-else
-    find "$GSH_HOME" -name ".$(gettext "coin")_?_*" -type f -print0 | xargs -0 rm -f
-    find "$(eval_gettext '$GSH_HOME/Garden')" -iname "*$(gettext "chest")*" -type f -print0 | xargs -0 rm -f
-    find "$(eval_gettext '$GSH_HOME/Garden')" -iname "*$(gettext "hut")*" -type f -print0 | xargs -0 rm -f
-    unset -f _mission_check
-    unset GSH_CHEST
-    false
-fi
+_mission_check 1 && _mission_check 2 && _mission_check 3
