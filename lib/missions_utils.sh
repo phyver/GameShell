e944caf7c16118ab375fcf46b546912361c7fdee
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-16 09:12:03 +0200

    use new function random_string to generate key in the caesar shift mission

--- lib/missions_utils.sh
+++ lib/missions_utils.sh
@@ -279,12 +279,12 @@ random_string() {
   local n=$1
 awk -v n=${n:-32} -v seed=$RANDOM 'BEGIN {
   srand(seed);
-  chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-  s = "";
+  l = split("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z", chars);
+  l = split("A B", chars);
   for(i=0;i<n;i++) {
-    s = s "" substr(chars, int(rand()*62), 1);
+    printf("%c", chars[1+int(rand()*l)]);
   }
-  print s
+printf("\n");
 }'
 }
 
