489112d123db7da32df3dd2057aafc41678dba0e
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-18 12:32:03 +0200

    bug in function random_string
    it only output A's and B's !

--- lib/missions_utils.sh
+++ lib/missions_utils.sh
@@ -300,7 +300,6 @@ random_string() {
 awk -v n=${n:-32} -v seed=$RANDOM 'BEGIN {
   srand(seed);
   l = split("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z", chars);
-  l = split("A B", chars);
   for(i=0;i<n;i++) {
     printf("%c", chars[1+int(rand()*l)]);
   }
