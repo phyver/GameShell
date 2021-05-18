1224fa8a4e9a162915e0e9ef64e1b1820dcff717
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-18 21:37:00 +0200

    cleaning

--- missions/stdin_stdout_stderr/04_stderr_dev-null_grimoires/init.sh
+++ missions/stdin_stdout_stderr/04_stderr_dev-null_grimoires/init.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-bib="$(eval_gettext '$GSH_HOME/Castle/Main_building/Library/Merlin_s_office')"
-find "$bib" -type f -name "$(gettext "grimoire")_*" -print0 | xargs -0 rm -f
+_mission_init() {
+  local bib="$(eval_gettext '$GSH_HOME/Castle/Main_building/Library/Merlin_s_office')"
+  find "$bib" -type f -name "$(gettext "grimoire")_*" -print0 | xargs -0 rm -f
 
-rm -f "$GSH_VAR/list_grimoires_RO"
+  rm -f "$GSH_VAR/list_grimoires_RO"
 
-_init_grimoires () {
+  local i
   for i in $(seq 100)
   do
-    file="$bib/$(gettext "grimoire")_$(CHECKSUM $RANDOM)"
+    local file="$bib/$(gettext "grimoire")_$(CHECKSUM $RANDOM)"
     random_string $((100 + RANDOM%600)) > "$file"
 
     if [ $(( RANDOM % 2 )) -eq 0 ]
@@ -19,14 +20,10 @@ _init_grimoires () {
     [ $((i%5)) -eq 0 ] && printf "."
   done
   printf "\n"
+  {
+    cd $bib
+    grep -il "pq" * 2> /dev/null | sort > $GSH_VAR/list_grimoires_PQ
+  }
 }
 
-_init_grimoires | progress_bar
-
-bash <<EOS
-  cd $bib
-  grep -il "pq" * 2> /dev/null | sort > $GSH_VAR/list_grimoires_PQ
-EOS
-
-unset -f _init_grimoires
-unset i file bib
+_mission_init | progress_bar
