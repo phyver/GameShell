d31270907cc730bf1ef79f21058044c6eccef75e
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-18 21:37:00 +0200

    cleaning

--- missions/basic/06_mv_coins_garden/check.sh
+++ missions/basic/06_mv_coins_garden/check.sh
@@ -2,47 +2,46 @@
 
 # Receives coin number as first argument.
 _mission_check() {
-    local COIN_NAME="$(gettext "coin")_$1"
+  local COIN_NAME="$(gettext "coin")_$1"
 
     # Check that the coin is not in the garden.
     if [ -f "$(eval_gettext '$GSH_HOME/Garden')/$COIN_NAME" ]
     then
-        echo "$(eval_gettext "The coin '\$COIN_NAME' is still in the garden!")"
-        return 1
+      echo "$(eval_gettext "The coin '\$COIN_NAME' is still in the garden!")"
+      return 1
     fi
 
     # Check that the coin is in the chest.
     if [ ! -f "$GSH_CHEST/$COIN_NAME" ]
     then
-        echo "$(eval_gettext "The coin '\$COIN_NAME' is not in the chest!")"
-        return 1
+      echo "$(eval_gettext "The coin '\$COIN_NAME' is not in the chest!")"
+      return 1
     fi
 
     # Check that the contents of the coin.
     if ! check_file "$GSH_CHEST/$COIN_NAME"
     then
-        echo "$(eval_gettext "The coin '\$COIN_NAME' has been tampered with...")"
-        return 1
+      echo "$(eval_gettext "The coin '\$COIN_NAME' has been tampered with...")"
+      return 1
     fi
 
     # check the coin.
     if ! check_file "$GSH_CHEST/$COIN_NAME"
     then
-        echo "$(eval_gettext "The coin '\$COIN_NAME' has been tampered with...")"
-        return 1
+      echo "$(eval_gettext "The coin '\$COIN_NAME' has been tampered with...")"
+      return 1
     fi
 
     return 0
-}
+  }
 
 if _mission_check 1 && _mission_check 2 && _mission_check 3
 then
-    unset -f _mission_check
-    true
+  true
 else
-    find "$GSH_HOME" -name "$(gettext "coin")_?" -type f -print0 | xargs -0 rm -f
-    find "$(eval_gettext '$GSH_HOME/Garden')" -iname "*$(gettext "chest")*" -type f -print0 | xargs -0 rm -f
-    find "$(eval_gettext '$GSH_HOME/Garden')" -iname "*$(gettext "hut")*" -type f -print0 | xargs -0 rm -f
-    unset -f _mission_check
-    false
+  #FIXME: use clean.sh
+  find "$GSH_HOME" -name "$(gettext "coin")_?" -type f -print0 | xargs -0 rm -f
+  find "$(eval_gettext '$GSH_HOME/Garden')" -iname "*$(gettext "chest")*" -type f -print0 | xargs -0 rm -f
+  find "$(eval_gettext '$GSH_HOME/Garden')" -iname "*$(gettext "hut")*" -type f -print0 | xargs -0 rm -f
+  false
 fi
