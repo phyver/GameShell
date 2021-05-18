81266f9a1e4be58b06fa4796f379277d5ccce518
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-18 15:04:58 +0200

    added a wheelbarrow in the shed

--- missions/finding_files_maze/00_shared/static.sh
+++ missions/finding_files_maze/00_shared/static.sh
@@ -4,4 +4,5 @@ mkdir -p "$(eval_gettext '$GSH_HOME/Garden/Shed')"
 
 sign_file "$MISSION_DIR/ascii-art/bucket.txt" "$(eval_gettext '$GSH_HOME/Garden/Shed')/$(gettext "bucket")"
 sign_file "$MISSION_DIR/ascii-art/flowers.txt" "$(eval_gettext '$GSH_HOME/Garden/Flower_garden')/$(gettext "flowers")"
+sign_file "$MISSION_DIR/ascii-art/wheelbarrow.txt" "$(eval_gettext '$GSH_HOME/Garden/Shed')/$(gettext "wheelbarrow")"
 
