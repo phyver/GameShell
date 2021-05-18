1a6e386d5b45db240f15bef16b857322c6f33550
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-18 21:46:44 +0200

    remove copy of book_of_potion in $GSH_VAR

--- missions/pipe_intro_book_of_potions/00_shared/clean.sh
+++ missions/pipe_intro_book_of_potions/00_shared/clean.sh
@@ -1,3 +1,4 @@
 rm -f "$(eval_gettext '$GSH_HOME/Mountain/Cave')/servillus"
 sign_file "$MISSION_DIR/ascii-art/cauldron.txt" "$(eval_gettext '$GSH_HOME/Mountain/Cave')/$(gettext "cauldron")"
 
+rm -rf "$GSH_VAR/book_of_potions"
