8ad9cc3a4f94166c65057f56a7eb63b68ff20eb2
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-19 09:37:11 +0200

    missing unset for local variables

--- missions/misc/03_tr_caesar_shift/check.sh
+++ missions/misc/03_tr_caesar_shift/check.sh
@@ -5,12 +5,12 @@ read -er dcode
 
 if [ "$dcode" = "$(cat "$GSH_VAR/secret_key")" ]
 then
-  unset dcode
   merlin_chest=$(eval_gettext '$GSH_HOME/Castle/Main_building/Library/Merlin_s_office')/$(gettext 'Merlin_s_chest')
   mkdir -p "$merlin_chest"
   cp "$(eval_gettext '$MISSION_DIR/secret_recipe/en.txt')" "$merlin_chest/$(gettext 'secret_recipe')"
   sign_file "$MISSION_DIR/ascii-art/medal.txt"  "$merlin_chest/$(gettext 'medal')"
   sign_file "$MISSION_DIR/ascii-art/bottle.txt"  "$merlin_chest/$(gettext 'bottle')"
+  unset merlin_chest dcode
   true
 else
   echo "$(gettext "That's not the secret key.")"
