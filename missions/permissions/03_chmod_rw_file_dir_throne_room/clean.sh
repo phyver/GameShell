80b16979c473be86b8af2b333cec730b27220b1e
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-21 15:15:23 +0200

    making sure tests pass on freebsd

--- missions/permissions/03_chmod_rw_file_dir_throne_room/clean.sh
+++ missions/permissions/03_chmod_rw_file_dir_throne_room/clean.sh
@@ -4,6 +4,6 @@
 safe="$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')/$(gettext "Safe")"
 mkdir -p "$safe"
 chmod 755 "$safe"
-chmod -f 644 "$safe/$(gettext "crown")"
+chmod -f 644 "$safe/$(gettext "crown")" 2>/dev/null
 rm -f "$GSH_VAR/crown"
 unset safe
