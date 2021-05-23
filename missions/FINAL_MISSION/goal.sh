fb8fd40e8c08e5dac922c1d2b92af08f0c6767e3
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-23 15:59:04 +0200

    simplify FINAL_MISSION

---
+++ missions/FINAL_MISSION/goal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export password=$(gettext "qwerty")
+checksum "$password" > "$GSH_CONFIG/admin_hash"
+envsubst '$password' < "$(eval_gettext '$MISSION_DIR/goal/en.txt')"
+unset password
+
