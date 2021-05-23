8c8ccb7bfc0c1acc3263f4faf85e02b2600f56e5
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-23 16:47:13 +0200

    replace double quotes by single quotes in some deps.sh messages

--- missions/basic/09_rm_wildcard_hidden_spiders_cellar/deps.sh
+++ missions/basic/09_rm_wildcard_hidden_spiders_cellar/deps.sh
@@ -1,7 +1,7 @@
 if ! [ -e "$MISSION_DIR/ascii-art" ]
 then
   DUMMY_MISSION=$(basename "$(dirname "$(REALPATH "$MISSION_DIR/ascii-art")")")
-  echo "$(eval_gettext 'Dummy mission "$DUMMY_MISSION" is required for mission $MISSION_NB ($MISSION_NAME).')"
+  echo "$(eval_gettext "Dummy mission '\$DUMMY_MISSION' is required for mission \$MISSION_NB (\$MISSION_NAME).")"
   unset DUMMY_MISSION
   false
 fi
