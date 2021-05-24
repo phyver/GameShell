3586f13467f4cdf7d372ed531f7c955eb3f34a52
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-24 08:26:26 +0200

    use sh for shell processes in group processes

--- missions/processes/01_ps_kill/init.sh
+++ missions/processes/01_ps_kill/init.sh
@@ -2,10 +2,9 @@
 
 if PYTHON_PATH=$(command -v python3)
 then
-    sed -e $'1c\\\n'"#!$PYTHON_PATH" "$MISSION_DIR/spell.py" > "$GSH_VAR/$(gettext "spell")"
+    { echo "#!$PYTHON_PATH" ; sed "1d" "$MISSION_DIR/spell.py" ; } > "$GSH_VAR/$(gettext "spell")"
 else
-    BASH_PATH=$(command -v bash)
-    sed -e $'1c\\\n'"#!$BASH_PATH" "$MISSION_DIR/spell.sh" > "$GSH_VAR/$(gettext "spell")"
+    cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
 fi
 chmod 755 "$GSH_VAR/$(gettext "spell")"
 "$GSH_VAR/$(gettext "spell")" &
