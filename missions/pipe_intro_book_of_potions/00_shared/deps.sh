beecb268f05604e8f004a06f2992f14262aca36a
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-24 14:20:30 +0200

    use missionname in a couple of scripts

--- missions/pipe_intro_book_of_potions/00_shared/deps.sh
+++ missions/pipe_intro_book_of_potions/00_shared/deps.sh
@@ -1,6 +1,6 @@
 if ! command -v install_potion_book.sh &> /dev/null
 then
-  DUMMY_MISSION=$(basename "$(dirname "$MISSION_DIR")")/00_shared
+  DUMMY_MISSION=$(missionname ../00_shared)
   echo "$(eval_gettext "Dummy mission '\$DUMMY_MISSION' is required for mission \$MISSION_NB (\$MISSION_NAME).")"
   unset DUMMY_MISSION
   false
