93cc4a151b9da2cda7aef11f1cc6ce24d8ed37c5
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-24 10:20:03 +0200

    better clean.sh in merchant_stall group

--- missions/pipes_merchant_stall/00_shared/clean.sh
+++ missions/pipes_merchant_stall/00_shared/clean.sh
@@ -1,11 +1,9 @@
-# need to be subtle, as there are so many files in the Stall that
+# need to be careful, as there are so many files in the Stall that
 # rm .../* may not work.
-# and find ... -exec rm is too slow
-rm -rf "$(eval_gettext '$GSH_HOME/Stall')"
-mkdir "$(eval_gettext '$GSH_HOME/Stall')"
-
-# this is to avoid a 'shell-init: error retrieving current directory'
-[ "$PWD" = "$(eval_gettext '$GSH_HOME/Stall')" ] && cd "$(eval_gettext '$GSH_HOME/Stall')"
+(
+  cd "$(eval_gettext '$GSH_HOME/Stall')"
+  find . -type f | xargs rm -f
+)
 
 rm -f "$GSH_VAR/nb_commands" "$GSH_VAR/last_command"
 
