1af55bf58e74a31748f9cd23878d1ba46e93554f
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-24 12:02:57 +0200

    cleaning for processes

--- missions/processes/02_ps_kill_signal/init.sh
+++ missions/processes/02_ps_kill_signal/init.sh
@@ -4,11 +4,7 @@ if PYTHON_PATH=$(command -v python3)
 then
     { echo "#!$PYTHON_PATH" ; sed "1d" "$MISSION_DIR/spell.py" ; } > "$GSH_VAR/$(gettext "spell")"
 else
-<<<<<<< Updated upstream
     cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
-=======
-    cp "$MISSION_DIR/spell.sh" > "$GSH_VAR/$(gettext "spell")"
->>>>>>> Stashed changes
 fi
 chmod 755 "$GSH_VAR/$(gettext "spell")"
 "$GSH_VAR/$(gettext "spell")" "$TEXTDOMAIN" &
--- missions/processes/03_pstree_kill/init.sh
+++ missions/processes/03_pstree_kill/init.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-<<<<<<< Updated upstream
-=======
+# this script uses 'disown' to prevent control job messages to appear on the
+# screen when cleaning.
+# I'm not sure if there is a way to do that in POSIX sh...
+
 BASH_PATH=$(command -v bash)
 
->>>>>>> Stashed changes
-cp "$MISSION_DIR/nice_fairy.sh" "$GSH_VAR/$(gettext "nice_fairy")"
+{ echo "#!$BASH_PATH" ; sed "1d" "$MISSION_DIR/nice_fairy.sh" ; } > "$GSH_VAR/$(gettext "nice_fairy")"
 chmod 755 "$GSH_VAR/$(gettext "nice_fairy")"
 
 mkdir -p "$GSH_VAR/fairy/"
 cp "$MISSION_DIR/fairy/spell.sh" "$GSH_VAR/fairy/$(gettext "spell")"
 chmod 755 "$GSH_VAR/fairy/$(gettext "spell")"
 "$GSH_VAR/$(gettext "nice_fairy")" &
-echo "$!" > "$GSH_VAR/fairy.pid"
+PID=$!
+disown "$PID"
+echo "$PID" > "$GSH_VAR/fairy.pid"
 
-cp "$MISSION_DIR/mischievous_imp.sh" "$GSH_VAR/$(gettext "mischievous_imp")"
+{ echo "#!$BASH_PATH" ; sed "1d" "$MISSION_DIR/mischievous_imp.sh" ; } > "$GSH_VAR/$(gettext "mischievous_imp")"
 chmod 755 "$GSH_VAR/$(gettext "mischievous_imp")"
 mkdir -p "$GSH_VAR/imp/"
 cp "$MISSION_DIR/imp/spell.sh" "$GSH_VAR/imp/$(gettext "spell")"
 chmod 755 "$GSH_VAR/imp/$(gettext "spell")"
 "$GSH_VAR/$(gettext "mischievous_imp")" &
-echo "$!" > "$GSH_VAR/imp.pid"
+PID=$!
+disown "$PID"
+echo "$PID" > "$GSH_VAR/imp.pid"
 
-unset BASH_PATH
+unset BASH_PATH PID
