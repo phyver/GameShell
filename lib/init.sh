c3a53ea1ce6232aa9e519417850b725be30c3663
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-05-25 23:21:46 +0200

    rename common.sh to init.sh
    and moved mission_source function to mission_source.sh file

---
+++ lib/init.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+[ -z "$GSH_ROOT" ] && echo "Error: GSH_ROOT undefined" && exit 1
+
+### defining GSH directories
+
+# first, resolve symbolic links in GSH_ROOT
+GSH_ROOT=$(cd "$GSH_ROOT" && pwd -P)
+
+# these directories should not be modified during a game
+export GSH_LIB="$GSH_ROOT/lib"
+export GSH_MISSIONS="$GSH_ROOT/missions"
+export GSH_UTILS="$GSH_ROOT/utils"
+
+# these directories should be erased when a new game is started, they only contain
+# dynamic data
+export GSH_HOME="$GSH_ROOT/World"
+export GSH_CONFIG="$GSH_ROOT/.config"
+export GSH_VAR="$GSH_ROOT/.var"
+export GSH_BASHRC="$GSH_ROOT/.bashrc"
+export GSH_BIN="$GSH_ROOT/.bin"
+export GSH_SBIN="$GSH_ROOT/.sbin"
+
+export TEXTDOMAINDIR="$GSH_ROOT/locale"
+export TEXTDOMAIN="gsh"
+
+# PATH=$PATH:"$GSH_ROOT/bin"
+PATH="$GSH_ROOT/bin":$PATH
+
+### generate GameShell translation files
+shopt -s nullglob
+for PO_FILE in "$GSH_ROOT"/i18n/*.po; do
+  PO_LANG=$(basename "$PO_FILE" .po)
+  MO_FILE="$GSH_ROOT/locale/$PO_LANG/LC_MESSAGES/$TEXTDOMAIN.mo"
+  if ! [ -f "$MO_FILE" ] || [ "$PO_FILE" -nt "$MO_FILE" ]
+  then
+    mkdir -p "$GSH_ROOT/locale/$PO_LANG/LC_MESSAGES"
+    msgfmt -o "$GSH_ROOT/locale/$PO_LANG/LC_MESSAGES/$TEXTDOMAIN.mo" "$PO_FILE"
+  fi
+done
+shopt -u nullglob
+
+
+### test some of the scripts
+if ! bash "$GSH_ROOT/lib/bin_test.sh"
+then
+  echo "$(gettext "Error: a least one base function is not working properly.
+Aborting!")"
+  exit 1
+fi
+
+
+# vim: shiftwidth=2 tabstop=2 softtabstop=2
