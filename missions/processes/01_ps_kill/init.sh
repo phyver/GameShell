a0e1e050e0319c242303d7553072c8e4d55d979b
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-05-28 01:40:54 +0200

    C version of the first two [spell] programs.
    (Not yet used by the missions.)

--- missions/processes/01_ps_kill/init.sh
+++ missions/processes/01_ps_kill/init.sh
@@ -2,23 +2,40 @@
 
 _mission_init() {
 
-  if ! [ -e "$MISSION_DIR/deps.sh" ]
+  if command -v c99 >/dev/null
+  then
+    CC=c99
+  elif command -v cc >/dev/null
+  then
+    CC=cc
+  elif command -v gcc >/dev/null
+  then
+    CC=gcc
+  elif command -v clang >/dev/null
+  then
+    CC=clang
+  elif ! [ -e "$MISSION_DIR/deps.sh" ]
   then
     # FIXME
     echo "missing dummy mission!"
     return 1
+    mission_source "$MISSION_DIR/deps.sh" || return 1
   fi
 
-  mission_source "$MISSION_DIR/deps.sh" || return 1
-
-  local PYTHON_PATH
-  if PYTHON_PATH=$(command -v python3)
+  if [ -n "$CC" ]
   then
-    { echo "#!$PYTHON_PATH" ; sed "1d" "$MISSION_DIR/spell.py" ; } > "$GSH_VAR/$(gettext "spell")"
-    else
-      cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
+    $CC "$MISSION_DIR/spell.c" -o "$GSH_VAR/$(gettext "spell")" || return 1
+    unset CC
+  else
+    local PYTHON_PATH
+    if PYTHON_PATH=$(command -v python3)
+    then
+      { echo "#!$PYTHON_PATH" ; sed "1d" "$MISSION_DIR/spell.py" ; } > "$GSH_VAR/$(gettext "spell")"
+      else
+        cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
+    fi
+    chmod 755 "$GSH_VAR/$(gettext "spell")"
   fi
-  chmod 755 "$GSH_VAR/$(gettext "spell")"
   "$GSH_VAR/$(gettext "spell")" &
   echo $! > "$GSH_VAR"/spell.pid
   disown
--- missions/processes/02_ps_kill_signal/init.sh
+++ missions/processes/02_ps_kill_signal/init.sh
@@ -2,24 +2,45 @@
 
 _mission_init() {
 
-  if ! [ -e "$MISSION_DIR/deps.sh" ]
+  local CC
+  if command -v c99 >/dev/null
+  then
+    CC=c99
+  elif command -v cc >/dev/null
+  then
+    CC=cc
+  elif command -v gcc >/dev/null
+  then
+    CC=gcc
+  elif command -v clang >/dev/null
+  then
+    CC=clang
+  elif ! [ -e "$MISSION_DIR/deps.sh" ]
   then
     # FIXME
     echo "missing dummy mission!"
     return 1
+    mission_source "$MISSION_DIR/deps.sh" || return 1
   fi
 
-  mission_source "$MISSION_DIR/deps.sh" || return 1
-
-  local PYTHON_PATH
-  if PYTHON_PATH=$(command -v python3)
+  if [ -n "$CC" ]
   then
-    { echo "#!$PYTHON_PATH" ; sed "1d" "$MISSION_DIR/spell.py" ; } > "$GSH_VAR/$(gettext "spell")"
-    else
-      cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
+    # under BSD, libintl is installed in /usr/local
+    $CC -lpthread "$MISSION_DIR/spell.c" -o "$GSH_VAR/$(gettext "spell")" 2>/dev/null ||
+    $CC -I/usr/local/include/ -L/usr/local/lib -lintl -lpthread "$MISSION_DIR/spell.c" -o "$GSH_VAR/$(gettext "spell")" 2>/dev/null ||
+    { echo "compilation failed" >&2; return 1; }
+  else
+    local PYTHON_PATH
+    if PYTHON_PATH=$(command -v python3)
+    then
+      { echo "#!$PYTHON_PATH" ; sed "1d" "$MISSION_DIR/spell.py" ; } > "$GSH_VAR/$(gettext "spell")"
+      else
+        cp "$MISSION_DIR/spell.sh" "$GSH_VAR/$(gettext "spell")"
+    fi
+    chmod 755 "$GSH_VAR/$(gettext "spell")"
   fi
-  chmod 755 "$GSH_VAR/$(gettext "spell")"
-  "$GSH_VAR/$(gettext "spell")" "$TEXTDOMAIN" &
+  TEXTDOMAIN=$TEXTDOMAIN "$GSH_VAR/$(gettext "spell")" &
+  echo $! > "$GSH_VAR"/spell.pids
   disown
   return 0
 }
