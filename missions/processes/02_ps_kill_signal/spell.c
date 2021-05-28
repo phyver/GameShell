a10c89afa9d8585f137e941754db26cd713108dd
Rodolphe Lepigre <lepigre@mpi-sws.org>
2021-05-29 00:49:11 +0200

    Improve the C process for [processes/02_ps_kill].
    - Use the TEXTDOMAIN variable instead of a command-line argument.
    - Use named semaphores to protect stdout and the file recording PIDs.

--- missions/processes/02_ps_kill_signal/spell.c
+++ missions/processes/02_ps_kill_signal/spell.c
@@ -5,11 +5,15 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
 
 #define NB_LINES 3
 #define MAX_SIZE 6
 #define MAX_SPACES 30
-#define SLEEP_TIME 5
+#define MIN_SLEEP_TIME 5
+#define MAX_SLEEP_BONUS 5
 
 char coal[NB_LINES][MAX_SIZE+1] = {
   " *#@*",
@@ -19,49 +23,80 @@ char coal[NB_LINES][MAX_SIZE+1] = {
 
 char spaces[MAX_SPACES+1] = "                              ";
 
-char **dom;
+char dom[1024];  // Plenty large enough for TEXTDOMAIN
 char path[1024]; // Plenty large enough for a file path.
 
+sem_t *printing_sem; // Semaphore protecting stdout.
+sem_t *writing_sem;  // Semaphore protecting the PID file.
+
+void print_coal() {
+  int err = sem_wait(printing_sem);
+
+  if(err != 0) return; // Might have been interrupted.
+
+  printf("\n");
+
+  char *s = spaces + (rand() % MAX_SPACES);
+  for(int i = 0; i < NB_LINES; i++) {
+    printf("%s%s\n", s, coal[i]);
+  }
+
+  printf("\n");
+
+  sem_post(printing_sem);
+}
+
+void write_pid(pid_t pid) {
+  sem_wait(writing_sem);
+
+  FILE *f = fopen(path, "a");
+  fprintf(f, "%d\n", pid);
+  fclose(f);
+
+  sem_post(writing_sem);
+}
+
 void spawn(int sig) {
-  printf("%s\n", dgettext(*dom,
+  printf("%s\n", dgettext(dom,
     "You'll need to do better than that to kill my spell!"));
 
   pid_t pid = fork();
 
   if(pid) {
-    FILE *f = fopen(path, "a"); // FIXME protect against concurrent writes?
-    fprintf(f, "%d\n", pid);
-    fclose(f);
+    write_pid(pid);
+
+    // Update the seed to desynchronize.
+    srand(time(NULL));
   }
 }
 
 int main(int argc, char *argv[]) {
   srand(time(NULL));
 
-  // Give access to the TEXTDOMAIN (given as first argument);
-  if(argc != 2) return 1;
-  dom = &argv[1];
+  // Give access to the TEXTDOMAIN environment variable.
+  char *dom_env = getenv("TEXTDOMAIN");
+  if(dom_env == NULL) return 1;
+  strncpy(dom, dom_env, 1024);
 
   // Get a path to the file in which to write the children's PIDs.
   char *gsh_var = getenv("GSH_VAR");
+  if(gsh_var == NULL) return 1;
   strncpy(path, gsh_var, 1024);
   if(strlen(path) > 1000) return 1;
   strcat(path, "/spell-term.pids");
+
+  // Initialize semaphores.
+  printing_sem = sem_open("/printing_sem", O_CREAT, 0644, 1);
+  if(printing_sem == SEM_FAILED) return 1;
+
+  writing_sem = sem_open("/writing_sem", O_CREAT, 0644, 1);
+  if(writing_sem == SEM_FAILED) return 1;
 
   // Register callback.
   signal(SIGTERM, spawn);
 
   while(1) {
-    sleep(SLEEP_TIME);
-
-    printf("\n");
-
-    char *s = spaces + (rand() % MAX_SPACES);
-
-    for(int i = 0; i < NB_LINES; i++) {
-      printf("%s%s\n", s, coal[i]);
-    }
-
-    printf("\n");
+    sleep(MIN_SLEEP_TIME + rand() % MAX_SLEEP_BONUS);
+    print_coal();
   }
 }
