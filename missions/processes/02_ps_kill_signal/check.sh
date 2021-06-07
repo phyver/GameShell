f874a66920438f2d460b9a209a6f6277614ade98
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-07 22:14:53 +0200

    bug when checking processes missions in narrow terminals
    GNU ps uses COLUMNS to truncate its output,
    even when output is not on a tty!
    As a result, the command name might get truncated!
    
    That's a little strange...
    
    I set COLUMNS to 512 to avoid that, but that's not very satisfying

--- missions/processes/02_ps_kill_signal/check.sh
+++ missions/processes/02_ps_kill_signal/check.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# we need to use ps -c to only get the command name and not the full command in
+# macOS
+# the command name is pretty long as it the processes are not in the path and
+# are given as absolute path
+# GNU ps truncates the output according to COLUMNS, even when output is not on
+# a tty, hence we set COLUMNS to 512 which should be long enough.
 _mission_check() {
-  local nb_spells=$(ps -ce | grep "$(gettext "spell")" | wc -l | tr -d ' ')
+  local nb_spells=$(COLUMNS=512 ps -ce | grep "$(gettext "spell")" | wc -l | tr -d ' ')
 
   if [ "$nb_spells" -gt 0 ]
   then
--- missions/processes/02_ps_kill_signal/test.sh
+++ missions/processes/02_ps_kill_signal/test.sh
@@ -1,11 +1,11 @@
-ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill 2> /dev/null
-ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill -9 2> /dev/null
+COLUMNS=512 ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill 2> /dev/null
+COLUMNS=512 ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill -9 2> /dev/null
 gsh assert check true
 
-ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill -9 2> /dev/null
+COLUMNS=512 ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill -9 2> /dev/null
 gsh assert check false
 
-ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill 2> /dev/null
-ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill 2> /dev/null
-ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | head -n2 | xargs kill -9 2> /dev/null
+COLUMNS=512 ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill 2> /dev/null
+COLUMNS=512 ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | xargs kill 2> /dev/null
+COLUMNS=512 ps -ce | awk -v spell="$(gettext "spell")" '$0 ~ spell {print $1}' | head -n2 | xargs kill -9 2> /dev/null
 gsh assert check false
