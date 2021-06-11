965b459e7fb49217308240fd2bf85bdf57b85a90
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-11 13:52:19 +0200

    openBSD doesn't have colors for ls
    the treasure that adds color to ls should not be used in this case

---
+++ missions/finding_files_maze/01_ls_cd/treasure-msg.sh
@@ -0,0 +1,14 @@
+if command ls --color >/dev/null 2>/dev/null
+then
+  # standard linux
+  cat "$(eval_gettext '$MISSION_DIR/treasure-msg/en.txt')"
+elif command ls -G >/dev/null 2>/dev/null
+then
+  # freebsd, macOS use another flag for colors: -G
+  cat "$(eval_gettext '$MISSION_DIR/treasure-msg/en.txt')"
+else
+  # openbsd doesn't use colors at all
+  :
+fi
+
+
