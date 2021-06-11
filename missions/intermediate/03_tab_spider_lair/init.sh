2ceeb361e4e97fd5d68741b8ebdb0e0cefdee082
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-11 13:41:21 +0200

    use smaller random parts in filenames
    some versions of tar choke on them!

--- missions/intermediate/03_tab_spider_lair/init.sh
+++ missions/intermediate/03_tab_spider_lair/init.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 _mission_init() {
-  local r1=$(checksum $RANDOM)
-  local r2=$(checksum $RANDOM)
+  local r1=$(random_string 16)
+  local r2=$(random_string 16)
   local lair="$(eval_gettext '$GSH_HOME/Castle/Cellar')/.$(gettext "Lair_of_the_spider_queen") ${r1} ${r2}"
   mkdir -p "$lair"
 
-  r1=$(checksum $RANDOM)
-  r2=$(checksum $RANDOM)
+  r1=$(random_string 16)
+  r2=$(random_string 16)
   local queen="${r1}_$(gettext "spider_queen")_$r2"
   sign_file "$MISSION_DIR/ascii-art/spider-queen.txt" "$lair/$queen"
 
-  r1=$(checksum $RANDOM)
-  r2=$(checksum $RANDOM)
+  r1=$(random_string 16)
+  r2=$(random_string 16)
   local bat="${r1}_$(gettext "baby_bat")_$r2"
   sign_file "$MISSION_DIR/ascii-art/baby-bat.txt" "$lair/$bat"
 
