8759a2020453e991a08c0b5d80629a39fe33b997
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-11 20:09:14 +0200

    allow using sed or awk (probably without pipe) in 05_pipe_head_tail

--- missions/pipe_intro_book_of_potions/05_pipe_head_tail/test.sh
+++ missions/pipe_intro_book_of_potions/05_pipe_head_tail/test.sh
@@ -16,6 +16,30 @@ history -d $((n-1))
 history -d $((n-1))
 unset n
 
+history -s "sed -n '4,6p' \"$(gettext "Book_of_potions")/$(gettext "page")_13\""
+history -s "gsh check"
+gsh assert check true
+n=$(history | tail -n1 | awk '{print $1}')
+history -d $((n-1))
+history -d $((n-1))
+unset n
+
+history -s "sed -n '3,6p' \"$(gettext "Book_of_potions")/$(gettext "page")_13\""
+history -s "gsh check"
+gsh assert check false
+n=$(history | tail -n1 | awk '{print $1}')
+history -d $((n-1))
+history -d $((n-1))
+unset n
+
+history -s "awk '4<=NR && NR<=6 {print}' \"$(gettext "Book_of_potions")/$(gettext "page")_13\""
+history -s "gsh check"
+gsh assert check true
+n=$(history | tail -n1 | awk '{print $1}')
+history -d $((n-1))
+history -d $((n-1))
+unset n
+
 history -s "head -n 6 \"$(gettext "Book_of_potions")/$(gettext 'page')_13\" | tail -n 3"
 history -s "something something"
 history -s "gsh check"
