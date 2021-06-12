24bc912a95f981cc568b0730b8c43c966c779eac
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-12 10:27:55 +0200

    bug when generating dates for paintings

--- missions/basic/12_cp_ls_mtime_paintings_tower/init.sh
+++ missions/basic/12_cp_ls_mtime_paintings_tower/init.sh
@@ -9,11 +9,11 @@ _mission_init() {
   local filename=$(mktemp "$(eval_gettext '$GSH_HOME/Castle/Main_tower/First_floor')"/$(gettext "painting")_XXXXXX)
   box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-pipe" > "$filename"
   local Y=$((1980 + RANDOM%10))
-  local M=$(echo 0$((1 + RANDOM%12)) | cut -c 1-2)
-  local D=$(echo 0$((1 + RANDOM%28)) | cut -c 1-2)
-  local h=$(echo 0$((RANDOM%24)) | cut -c 1-2)
-  local m=$(echo 0$((RANDOM%60)) | cut -c 1-2)
-  local s=$(echo 0$((RANDOM%60)) | cut -c 1-2)
+  local M=$(printf "%02d" $((1 + RANDOM%12)))
+  local D=$(printf "%02d" $((1 + RANDOM%28)))
+  local h=$(printf "%02d" $((RANDOM%24)))
+  local m=$(printf "%02d" $((RANDOM%60)))
+  local s=$(printf "%02d" $((RANDOM%60)))
   echo "$(basename "$filename")" > "$GSH_VAR/painting"
   sign_file "$filename"
   touch -t "$Y$M$D$h$m.$s" "$filename"
@@ -27,11 +27,11 @@ _mission_init() {
   box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-joconde" > "$filename"
   Y=$(date +%Y)
   Y=$((Y + 1 + RANDOM%10))
-  M=$(echo 0$((1 + RANDOM%12)) | cut -c 1-2)
-  D=$(echo 0$((1 + RANDOM%28)) | cut -c 1-2)
-  h=$(echo 0$((RANDOM%24)) | cut -c 1-2)
-  m=$(echo 0$((RANDOM%60)) | cut -c 1-2)
-  s=$(echo 0$((RANDOM%60)) | cut -c 1-2)
+  M=$(printf "%02d" $((1 + RANDOM%12)))
+  D=$(printf "%02d" $((1 + RANDOM%28)))
+  h=$(printf "%02d" $((RANDOM%24)))
+  m=$(printf "%02d" $((RANDOM%60)))
+  s=$(printf "%02d" $((RANDOM%60)))
   sign_file "$filename"
   touch -t "$Y$M$D$h$m.$s" "$filename"
 }
