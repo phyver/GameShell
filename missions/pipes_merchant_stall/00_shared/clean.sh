c5af4f8e38c444fc6ae96860507dd1cc3284d731
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-14 23:39:23 +0200

    convert count_commands.sh script to POSIX

--- missions/pipes_merchant_stall/00_shared/clean.sh
+++ missions/pipes_merchant_stall/00_shared/clean.sh
@@ -9,8 +9,8 @@
 
 rm -f "$GSH_VAR/nb_commands" "$GSH_VAR/last_command"
 
-PROMPT_COMMAND=$OLD_PROMPT_COMMAND
-unset OLD_PROMPT_COMMAND
+PS1=$_PS1
+unset _PS1
 unset -f _cmd
 
 rm -f "$GSH_VAR/nbUnpaid" "$GSH_VAR/amountKing"
--- missions/pipes_merchant_stall/00_shared/count_commands.sh
+++ missions/pipes_merchant_stall/00_shared/count_commands.sh
@@ -1,41 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 
-# the ``history`` command is not in POSIX but it might be possible to use fc
-# instead.
-# PROMPT_COMMAND is only bash
-# FIXME: we could try relying on fc' indices to avoid relying no
-# PROMPT_COMMAND
+# NOTE: debian's sh (dash) is compiled without fc, so this won't work!
 
 echo 0 > "$GSH_VAR/nb_commands"
-history -a
 fc -l | tail -n 1 | awk '{print $1}' > "$GSH_VAR/last_command"
 
-_cmd() {
+_count_cmd() (
   touch "$HISTFILE"
-  history -a
-
   [ -e "$GSH_VAR/nb_commands" ] || return
 
-  local last_cmd_1=$(fc -l | tail -n 1 | awk '{print $1}')
-  local last_cmd_2=$(cat "$GSH_VAR/last_command")
+  last_cmd_1=$(fc -l | tail -n 1 | awk '{print $1}')
+  last_cmd_2=$(cat "$GSH_VAR/last_command")
   echo "$last_cmd_1" > "$GSH_VAR/last_command"
 
-  local pc=$(tail -n1 "$HISTFILE")
-  local nb_cmd=$(cat "$GSH_VAR/nb_commands")
+  pc=$(fc -l | tail -n 1 | awk '{print $2}')
+  nb_cmd=$(cat "$GSH_VAR/nb_commands")
 
   if [ "$last_cmd_1" != "$last_cmd_2" ] && [ -n "$pc" ] && ! echo "$pc" | grep -x '\s*gsh\s*\w*\s*' &>/dev/null
   then
     nb_cmd=$((nb_cmd+1))
     echo $nb_cmd > "$GSH_VAR/nb_commands"
   fi
-  echo "($nb_cmd)"
-}
-export -f _cmd
+  echo "($nb_cmd) "
+)
 
-PROMPT_COMMAND=$(echo "$PROMPT_COMMAND" | sed 's|[[:blank:]]*;\?[[:blank:]]*_cmd.*||')
-if [ -z "$PROMPT_COMMAND" ]
-then
-    PROMPT_COMMAND="_cmd"
-else
-    PROMPT_COMMAND="$PROMPT_COMMAND;_cmd"
-fi
+_PS1=$PS1
+PS1='$(_count_cmd)'$PS1
