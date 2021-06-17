a08ec88519d06b24649f90a1f1359a0fe3838ad0
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-17 08:32:41 +0200

    better clean.sh in permission group

--- missions/permissions/03_chmod_rw_file_dir_throne_room/clean.sh
+++ missions/permissions/03_chmod_rw_file_dir_throne_room/clean.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-# avoid problems when making a tar archive
 safe="$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')/$(gettext "Safe")"
-mkdir -p "$safe"
+
+case "$GSH_LAST_ACTION" in
+  "check_true")
+    :
+    ;;
+  *)
+    _PWD="$(realpath $(pwd) 2>/dev/null)"
+    if [ -n "$_PWD" ] && [ "$_PWD" = "$(realpath "$safe" 2>/dev/null)" ]
+    then
+      cd "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room')"
+    fi
+    unset _PWD
+    ;;
+esac
+
+# avoid problems when making a tar archive
 chmod 755 "$safe"
 chmod -f 644 "$safe/$(gettext "crown")" 2>/dev/null
 rm -f "$GSH_VAR/crown"
