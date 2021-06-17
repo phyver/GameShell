d352f5c94121a725e690a61820932217fcc59a51
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-17 08:32:41 +0200

    better clean.sh in permission group

--- missions/permissions/01_chmod_x_dir_king_quarter/clean.sh
+++ missions/permissions/01_chmod_x_dir_king_quarter/clean.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+king_quarter=$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room/Kings_quarter')
+
+case "$GSH_LAST_ACTION" in
+  "check_true")
+    :
+    ;;
+  *)
+    _PWD="$(realpath $(pwd) 2>/dev/null)"
+    if [ -n "$_PWD" ] && [ "$_PWD" = "$(realpath "$king_quarter" 2>/dev/null)" ]
+    then
+      cd ..
+    fi
+    unset _PWD
+    ;;
+esac
+
 # avoid problems when making a tar archive
-chmod +x "$(eval_gettext '$GSH_HOME/Castle/Main_building/Throne_room/Kings_quarter')"
+chmod +x "$king_quarter"
+unset king_quarter
