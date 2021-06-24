0291dfa27d0040d5001163595de4fb1ebff4e0bd
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-24 14:02:29 +0200

    .gshrc from treasure in 02_alias_journal is now stored in $GSH_HOME

--- missions/intermediate/02_alias_journal/gshrc/en.sh
+++ missions/intermediate/02_alias_journal/gshrc/en.sh
@@ -8,11 +8,14 @@
 # Standard shell use such files, usually hidden in the home
 # directory. For bash, the file is found at the path
 #       ~/.bashrc
+# and the one for zsh at
+#       ~/.zshrc
+# (Do not modify those files as they are used by GameShell!)
 
 # Whenever you edit this file, the definitions it contains are not
 # directly added to the current shell. You can however rely on the
 # following alias to do that for you.
-alias reload="source $GSH_CHEST/gshrc"
+alias reload="source ~/.gshrc"
 
 # Here are a few alias ideas for you:
 # - an alias "gg" for "gsh goal",
--- missions/intermediate/02_alias_journal/treasure.sh
+++ missions/intermediate/02_alias_journal/treasure.sh
@@ -2,12 +2,12 @@
 
 alias "$(gettext "journal")"="nano '$GSH_CHEST/$(gettext "journal").txt'"
 
-if [ ! -f "$GSH_CHEST/gshrc" ]
+if [ ! -f "$GSH_HOME/.gshrc" ]
 then
-  cp "$(eval_gettext '$MISSION_DIR/gshrc/en.sh')" "$GSH_CHEST/gshrc"
+  cp "$GSH_CONFIG/player.gshrc" "$GSH_HOME/.gshrc"
 fi
 
-if [ -f "$GSH_CHEST/gshrc" ]
+if [ -f "$GSH_HOME/.gshrc" ]
 then
-  . "$GSH_CHEST/gshrc"
+  . "$GSH_HOME/.gshrc"
 fi
