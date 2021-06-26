8a7e7e4f213ab465566c69a1d64de52fd1c007e8
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-26 15:51:22 +0200

    use my_ps command in processes test

--- missions/processes/00_shared/init.sh
+++ missions/processes/00_shared/init.sh
@@ -7,7 +7,7 @@ _mission_init() (
   chmod +x "$GSH_TMP/test-proc-name"
   "$GSH_TMP/test-proc-name" &
   PID=$!
-  name=$(ps -cp $PID | grep $PID | grep -v sh)
+  name=$(my_ps $PID | grep $PID | grep -v sh)
   kill -9 $PID
   if [ -z "$name" ]
   then
