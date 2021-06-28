b2fec226eebec24ebe979f74adeaebff0a4b47f0
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-06-28 09:05:15 +0200

    change directory in 04_mkdir_chest/clean.sh if the player is in the Hut
    (because we remove this directory)

--- missions/basic/04_mkdir_chest/clean.sh
+++ missions/basic/04_mkdir_chest/clean.sh
@@ -5,6 +5,15 @@ case $GSH_LAST_ACTION in
     :
     ;;
   *)
+    case "$(pwd)" in
+      "$GSH_HOME"/*)
+        if echo "${PWD#$GSH_HOME/}" | grep -Eq "$(gettext "Hut")|$(gettext "Chest")"
+        then
+          cd "$GSH_HOME"
+          echo "$(gettext "You are back at the initial directory.")"
+        fi
+        ;;
+    esac
     find "$GSH_HOME" -iname "$(gettext "Hut")" -print0 | xargs -0 rm -rf
     find "$GSH_HOME" -iname "$(gettext "Chest")" -print0 | xargs -0 rm -rf
     ;;
