ef3c11567dc39de04760b17f78b4da6a56f835bb
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-07-01 17:58:38 +0200

    do not set modification time in the future (mission 12)
    otherwise, tar complains about it...

--- missions/basic/12_cp_ls_mtime_paintings_tower/init.sh
+++ missions/basic/12_cp_ls_mtime_paintings_tower/init.sh
@@ -8,39 +8,49 @@ _mission_init() (
 
   cd "$(eval_gettext '$GSH_HOME/Castle/Main_tower/First_floor')"
 
+  i=$(RANDOM)
+  i=$((1+i%3))
+
   while true
   do
     filename="$(gettext "painting")_$(random_string 8)"
     [ -e "$filename" ] || break
   done
-  box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-pipe" > "$filename"
+  box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-$i" > "$filename"
   Y=$((1980 + $(RANDOM)%10))
   M=$(printf "%02d" $((1 + $(RANDOM)%12)))
   D=$(printf "%02d" $((1 + $(RANDOM)%28)))
   h=$(printf "%02d" $(($(RANDOM)%24)))
   m=$(printf "%02d" $(($(RANDOM)%60)))
   s=$(printf "%02d" $(($(RANDOM)%60)))
-  echo "$(basename "$filename")" > "$GSH_TMP/painting"
+  basename "$filename" > "$GSH_TMP/painting"
   sign_file "$filename"
   touch -t "$Y$M$D$h$m.$s" "$filename"
 
+  i=$((1+i%3))
   while true
   do
     filename="$(gettext "painting")_$(random_string 8)"
     [ -e "$filename" ] || break
   done
-  box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-star_wars" > "$filename"
+  box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-$i" > "$filename"
+  Y=$((1995 + $(RANDOM)%10))
+  M=$(printf "%02d" $((1 + $(RANDOM)%12)))
+  D=$(printf "%02d" $((1 + $(RANDOM)%28)))
+  h=$(printf "%02d" $(($(RANDOM)%24)))
+  m=$(printf "%02d" $(($(RANDOM)%60)))
+  s=$(printf "%02d" $(($(RANDOM)%60)))
   sign_file "$filename"
-  touch "$filename"
+  touch -t "$Y$M$D$h$m.$s" "$filename"
 
+  i=$((1+i%3))
   while true
   do
     filename="$(gettext "painting")_$(random_string 8)"
     [ -e "$filename" ] || break
   done
-  box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-joconde" > "$filename"
-  Y=$(date +%Y)
-  Y=$((Y + 1 + $(RANDOM)%10))
+  box.sh -B Diamond "$MISSION_DIR/ascii-art/painting-$i" > "$filename"
+  Y=$((2010 + $(RANDOM)%10))
   M=$(printf "%02d" $((1 + $(RANDOM)%12)))
   D=$(printf "%02d" $((1 + $(RANDOM)%28)))
   h=$(printf "%02d" $(($(RANDOM)%24)))
