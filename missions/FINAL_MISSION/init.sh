3a5455d637627a454816301d91bfafc45436aae5
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-07-28 10:38:24 +0200

    final mission will automatically exit GameShell in debug mode
    it will also run "gsh stat" before exiting

--- missions/FINAL_MISSION/init.sh
+++ missions/FINAL_MISSION/init.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-parchment -B Twinkle "$(eval_gettext '$MISSION_DIR/msg/en.txt')"
+if [ "$GSH_MODE" = DEBUG ]
+then
+  color_echo green "$(eval_gettext "CONGRATULATION, you've finished the game!")"
+  echo
+  gsh stat
+  gsh exit
+else
+  parchment -B Twinkle "$(eval_gettext '$MISSION_DIR/msg/en.txt')"
+fi
