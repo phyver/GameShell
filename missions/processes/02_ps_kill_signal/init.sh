40be173749caece815e0c2491bfbc3e965dbb49d
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2021-08-26 11:07:14 +0200

    fix init.sh for processes group
    if a compiler was found, but compilation failed, the mission was
    cancelled rather than falling back to using the shell scripts.

--- missions/processes/02_ps_kill_signal/init.sh
+++ missions/processes/02_ps_kill_signal/init.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-_mission_init() {
+_compile() (
 
   if command -v gcc >/dev/null
   then
@@ -22,50 +22,58 @@ _mission_init() {
   elif command -v cc >/dev/null
   then
     CC=cc
-  elif ! [ -e "$MISSION_DIR/deps.sh" ]
+  else
+    return 1
+  fi
+
+  (
+    # in debug mode, don't hide messages
+    if [ "$GSH_MODE" != DEBUG ] || [ -z "$GSH_VERBOSE_DEBUG" ]
+    then
+      exec 1>/dev/null
+      exec 2>/dev/null
+    fi
+
+    # under BSD, libintl is installed in /usr/local and we need to pass
+    # "-lintl" to the compiler, so we have to try several things!
+    {
+      echo "GSH: compiling spell.c, first try" >&2
+    echo $CC $CFLAGS "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/$(gettext "spell")"
+      $CC $CFLAGS "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/$(gettext "spell")"
+    } ||
+    {
+      echo "GSH: compiling spell.c, second try"
+      echo $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/spell.c" -lintl -lpthread -o "$GSH_TMP/$(gettext "spell")"
+      $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/spell.c" -lintl -lpthread -o "$GSH_TMP/$(gettext "spell")"
+    }
+  ) || { echo "compilation failed" >&2; return 1; }
+  return 1
+)
+
+_install_script() (
+
+  if ! [ -e "$MISSION_DIR/deps.sh" ] || ! command -v my_ps >/dev/null
   then
     # FIXME
     echo "missing dummy mission!"
     return 1
-    mission_source "$MISSION_DIR/deps.sh" || return 1
   fi
+  mission_source "$MISSION_DIR/deps.sh" || return 1
+  cp "$MISSION_DIR/spell.sh" "$GSH_TMP/$(gettext "spell")"
+  chmod 755 "$GSH_TMP/$(gettext "spell")"
 
-  if [ -n "$CC" ]
-  then
+)
 
-    (
-      # in debug mode, don't hide messages
-      if [ "$GSH_MODE" != DEBUG ] || [ -z "$GSH_VERBOSE_DEBUG" ]
-      then
-        exec 1>/dev/null
-        exec 2>/dev/null
-      fi
+if _compile || _install_script
+then
+  unset -f _compile _install_script
+  [ -n "$GSH_NON_INTERACTIVE" ] || set +o monitor  # do not monitor background processes
+  # FIXME: for some unknown reason, this doesn't work if we start with this
+  # mission directly!
 
-      # under BSD, libintl is installed in /usr/local and we need to pass
-      # "-lintl" to the compiler, so we have to try several things!
-      {
-        echo "GSH: compiling spell.c, first try" >&2
-        echo $CC $CFLAGS "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/$(gettext "spell")"
-        $CC $CFLAGS "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/$(gettext "spell")"
-      } ||
-      {
-        echo "GSH: compiling spell.c, second try"
-        echo $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/spell.c" -lintl -lpthread -o "$GSH_TMP/$(gettext "spell")"
-        $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/spell.c" -lintl -lpthread -o "$GSH_TMP/$(gettext "spell")"
-      }
-    ) || { echo "compilation failed" >&2; return 1; }
-
-  else
-    cp "$MISSION_DIR/spell.sh" "$GSH_TMP/$(gettext "spell")"
-    chmod 755 "$GSH_TMP/$(gettext "spell")"
-  fi
   "$GSH_TMP/$(gettext "spell")" &
   echo $! > "$GSH_TMP"/spell.pids
-  return 0
-}
-
-[ -n "$GSH_NON_INTERACTIVE" ] || set +o monitor  # do not monitor background processes
-# FIXME: for some unknown reason, this doesn't work if we start with this
-# mission directly!
-
-_mission_init
+else
+  unset -f _compile _install_script
+  false
+fi
--- missions/processes/03_pstree_kill/init.sh
+++ missions/processes/03_pstree_kill/init.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-_mission_init() {
+_compile() (
+
+  if ! command -v pstree >/dev/null
+  then
+    echo "$(eval_gettext "The command 'pstree' is required for mission \$MISSION_NAME.
+(Debian / Ubuntu: install package 'psmisc')")" >&2
+    return 1
+  fi
 
   if command -v gcc >/dev/null
   then
@@ -22,83 +29,82 @@ _mission_init() {
   elif command -v cc >/dev/null
   then
     CC=cc
-  elif ! [ -e "$MISSION_DIR/deps.sh" ]
+  else
+    return 1
+  fi
+
+  (
+    if [ "$GSH_MODE" != DEBUG ] || [ -z "$GSH_VERBOSE_DEBUG" ]
+    then
+      exec 1>/dev/null
+      exec 2>/dev/null
+    fi
+
+    # under BSD, libintl is installed in /usr/local
+    {
+      echo "GSH: compiling process.c, first try" >&2
+      echo $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "nice_fairy")"
+      $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "nice_fairy")"
+      echo $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "mischievous_imp")"
+      $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "mischievous_imp")"
+    } ||
+    {
+      echo "GSH: compiling process.c, second try"
+      echo $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "nice_fairy")"
+      $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "nice_fairy")"
+      echo $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "mischievous_imp")"
+      $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "mischievous_imp")"
+    } || { echo "compilation failed" >&2; return 1; }
+
+    mkdir -p "$GSH_TMP/fairy/"
+    $CC $CFLAGS -D WHO=FAIRY "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/fairy/$(gettext "spell")"
+
+    mkdir -p "$GSH_TMP/imp/"
+    $CC $CFLAGS -D WHO=IMP "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/imp/$(gettext "spell")"
+  )
+
+)
+
+_install_script() (
+
+  if ! [ -e "$MISSION_DIR/deps.sh" ] || ! command -v my_ps >/dev/null
   then
     # FIXME
     echo "missing dummy mission!"
     return 1
-    mission_source "$MISSION_DIR/deps.sh" || return 1
   fi
+  mission_source "$MISSION_DIR/deps.sh" || return 1
 
-  if ! command -v pstree >/dev/null
-  then
-    echo "$(eval_gettext "The command 'pstree' is required for mission \$MISSION_NAME.
-(Debian / Ubuntu: install package 'psmisc')")" >&2
-    return 1
+  cp "$MISSION_DIR/nice_fairy.sh" "$GSH_TMP/$(gettext "nice_fairy")"
+  chmod 755 "$GSH_TMP/$(gettext "nice_fairy")"
 
-  fi
+  mkdir -p "$GSH_TMP/fairy/"
+  cp "$MISSION_DIR/fairy/spell.sh" "$GSH_TMP/fairy/$(gettext "spell")"
+  chmod 755 "$GSH_TMP/fairy/$(gettext "spell")"
 
-  if [ -n "$CC" ]
-  then
-    (
-      if [ "$GSH_MODE" != DEBUG ] || [ -z "$GSH_VERBOSE_DEBUG" ]
-      then
-        exec 1>/dev/null
-        exec 2>/dev/null
-      fi
+  cp "$MISSION_DIR/mischievous_imp.sh" "$GSH_TMP/$(gettext "mischievous_imp")"
+  chmod 755 "$GSH_TMP/$(gettext "mischievous_imp")"
 
-      # under BSD, libintl is installed in /usr/local
-      {
-        echo "GSH: compiling process.c, first try" >&2
-        echo $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "nice_fairy")"
-        $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "nice_fairy")"
-        echo $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "mischievous_imp")"
-        $CC $CFLAGS "$MISSION_DIR/process.c" -o "$GSH_TMP/$(gettext "mischievous_imp")"
-      } ||
-      {
-        echo "GSH: compiling process.c, second try"
-        echo $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "nice_fairy")"
-        $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "nice_fairy")"
-        echo $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "mischievous_imp")"
-        $CC $CFLAGS -I/usr/local/include/ -L/usr/local/lib "$MISSION_DIR/process.c" -lintl -o "$GSH_TMP/$(gettext "mischievous_imp")"
-      } || { echo "compilation failed" >&2; return 1; }
+  mkdir -p "$GSH_TMP/imp/"
+  cp "$MISSION_DIR/imp/spell.sh" "$GSH_TMP/imp/$(gettext "spell")"
+  chmod 755 "$GSH_TMP/imp/$(gettext "spell")"
+)
 
-      mkdir -p "$GSH_TMP/fairy/"
-      $CC $CFLAGS -D WHO=FAIRY "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/fairy/$(gettext "spell")"
 
-      mkdir -p "$GSH_TMP/imp/"
-      $CC $CFLAGS -D WHO=IMP "$MISSION_DIR/spell.c" -lpthread -o "$GSH_TMP/imp/$(gettext "spell")"
-    )
-    unset CC
+if _compile || _install_script
+then
+  unset -f _compile _install_script
 
-  else
-
-    cp "$MISSION_DIR/nice_fairy.sh" "$GSH_TMP/$(gettext "nice_fairy")"
-    chmod 755 "$GSH_TMP/$(gettext "nice_fairy")"
-
-    mkdir -p "$GSH_TMP/fairy/"
-    cp "$MISSION_DIR/fairy/spell.sh" "$GSH_TMP/fairy/$(gettext "spell")"
-    chmod 755 "$GSH_TMP/fairy/$(gettext "spell")"
-
-    cp "$MISSION_DIR/mischievous_imp.sh" "$GSH_TMP/$(gettext "mischievous_imp")"
-    chmod 755 "$GSH_TMP/$(gettext "mischievous_imp")"
-
-    mkdir -p "$GSH_TMP/imp/"
-    cp "$MISSION_DIR/imp/spell.sh" "$GSH_TMP/imp/$(gettext "spell")"
-    chmod 755 "$GSH_TMP/imp/$(gettext "spell")"
-  fi
+  [ -n "$GSH_NON_INTERACTIVE" ] || set +o monitor  # do not monitor background processes
+  # FIXME: for some unknown reason, this doesn't work if we start with this
+  # mission directly!
 
   "$GSH_TMP/$(gettext "nice_fairy")" &
   echo $! > "$GSH_TMP/fairy.pid"
 
   "$GSH_TMP/$(gettext "mischievous_imp")" &
   echo $! > "$GSH_TMP/imp.pid"
-
-  return 0
-}
-
-[ -n "$GSH_NON_INTERACTIVE" ] || set +o monitor  # do not monitor background processes
-# FIXME: for some unknown reason, this doesn't work if we start with this
-# mission directly!
-
-_mission_init
+else
+  unset -f _compile _install_script
+  false
+fi
