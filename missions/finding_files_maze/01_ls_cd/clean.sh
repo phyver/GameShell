b1ecfdce971e35fca897c535c470707ea9c5e876
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2022-03-30 11:30:23 +0200

    unprotect / protect the ROOT directory in finding_files_maze/01_ls_cd
    otherwise, the maze directory is not accessible with the graphical file
    manager for those who want to use it

--- missions/finding_files_maze/01_ls_cd/clean.sh
+++ missions/finding_files_maze/01_ls_cd/clean.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# protect the root directory
+[ "$GSH_MODE" != "DEBUG" ] && ! [ -d "$GSH_ROOT/.git" ] && gsh protect
+
 . "$MISSION_DIR/../00_shared/clean.sh"
 rm -f "$GSH_TMP/copper_coin"
