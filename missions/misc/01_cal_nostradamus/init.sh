260550bd38e11b4ee4c851324061716a5d8d3992
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2022-05-26 16:12:26 +0200

    keep dates below 2038 for compatibility with 32bits systems

--- missions/misc/01_cal_nostradamus/init.sh
+++ missions/misc/01_cal_nostradamus/init.sh
@@ -6,8 +6,10 @@ _mission_init() (
 (Debian / Ubuntu: install package 'bsdmainutils')")" >&2
     return 1
   fi
-  YYYY=$((1900 + $(RANDOM) % 300))
+  # keep the date between 1902 and 2038 to avoid problems with 32bits systems
+  YYYY=$((1902 + $(RANDOM) % 136))
   MM=$(printf "%02d" "$((1 + $(RANDOM) % 12))")
+  # keep the day strictly above 12, to avoid confusing day/month
   DD=$(printf "%02d" "$((13 + $(RANDOM) % 15))")
   echo "$YYYY-$MM-$DD" > "$GSH_TMP/date"
 )
