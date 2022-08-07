4f641ba058dbb672b8f12fd92ac439f9e3cf04be
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2022-08-07 13:42:12 +0200

    change to Stall directory in auto scripts
    since the Stall directory is removed by the clean.sh script, it helps
    detect everything works correctly

--- missions/pipes_merchant_stall/01_pipe_1/auto.sh
+++ missions/pipes_merchant_stall/01_pipe_1/auto.sh
@@ -2,6 +2,8 @@
 #
 # FIXME? I use bash' process substitution to avoid creating a subshell
 
+cd "$(eval_gettext '$GSH_HOME/Stall')"
+
 # beware, there are so many files that we may get "bash: /usr/bin/grep: Argument list too long"
 # so we use recursive grep on the diectory instead of globbing
 gsh check < <(grep -r "$(gettext "the King")" "$(eval_gettext '$GSH_HOME/Stall')" |
--- missions/pipes_merchant_stall/02_pipe_2/auto.sh
+++ missions/pipes_merchant_stall/02_pipe_2/auto.sh
@@ -2,6 +2,8 @@
 
 # FIXME? I use bash' process substitution to avoid creating a subshell
 
+cd "$(eval_gettext '$GSH_HOME/Stall')"
+
 # beware, there are so many files that we may get "bash: /usr/bin/grep: Argument list too long"
 # so we use recursive grep on the directory instead of globbing
 gsh check < <(grep -rv "$(gettext "PAID")" "$(eval_gettext '$GSH_HOME/Stall')" | wc -l)
