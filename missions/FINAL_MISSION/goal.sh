6ff32738fc64cdbc072d12e08ec4b557efbb0ede
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2023-06-13 21:21:24 +0200

    bug when changing the admin password in the last mission
    this mission wasn't updated when the salt was introduced in commit cb69b40fb1f1f3c58772290bf526a2175e4d0755

--- missions/FINAL_MISSION/goal.sh
+++ missions/FINAL_MISSION/goal.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-export password=$(gettext "qwerty")
-checksum "$password" > "$GSH_CONFIG/admin_hash"
-sed "s/\\\$password/$password/g" "$(eval_gettext '$MISSION_DIR/goal/en.txt')"
-unset password
+ADMIN_PASSWD=$(gettext "qwerty")
 
+ADMIN_SALT=$("$GSH_ROOT"/scripts/random_string)
+ADMIN_HASH=$(checksum "$ADMIN_SALT $ADMIN_PASSWD")
+
+echo "$ADMIN_SALT" > "$GSH_CONFIG/admin_salt"
+echo "$ADMIN_HASH" > "$GSH_CONFIG/admin_hash"
+
+sed "s/\\\$password/$ADMIN_PASSWD/g" "$(eval_gettext '$MISSION_DIR/goal/en.txt')"
+unset ADMIN_PASSWD ADMIN_SALT ADMIN_HASH
