7785859ae9597f10fef505d1d5e9f68da3cdb271
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2024-05-08 15:02:59 +0200

    French translations for control-C

--- missions/intermediate/05_control-C/init.sh
+++ missions/intermediate/05_control-C/init.sh
@@ -4,7 +4,6 @@ _mission_init() {
   # nb of tries to make
   echo $((3 + $(RANDOM) % 3)) > "$GSH_TMP/control-C"
 
-  cp "$MISSION_DIR/charmiglio.sh" "$GSH_BIN/$(gettext charmiglio)"
-  chmod +x "$GSH_BIN/$(gettext charmiglio)"
+  copy_bin "$MISSION_DIR/charmiglio.sh" "$GSH_BIN/$(gettext charmiglio)"
 }
 _mission_init
