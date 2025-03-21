2c670f9086f828323646ed6eeaf7438033690952
Pierre Hyvernat <pierre.hyvernat@univ-smb.fr>
2025-03-21 10:27:22 +0100

    allow player to use gsh commands in mission 03_cd_HOME_throne
    it is suprising that
     cd
     cd .....
     gsh goal
     gsh check
    doesn't work because of the gsh goal

--- missions/basic/03_cd_HOME_throne/check.sh
+++ missions/basic/03_cd_HOME_throne/check.sh
@@ -6,7 +6,7 @@ _mission_check() {
   goal=$(readlink-f "$(eval_gettext "\$GSH_HOME/Castle/Main_building/Throne_room")")
   current=$(readlink-f "$PWD")
 
-  ppc=$(. fc-lnr.sh | sed -n '2p;3q')
+  ppc=$(. fc-lnr.sh | grep -v '^[[:blank:]]*gsh' | sed -n '2p;3q')
 
   # FIXME: also accept other commands to go back to the starting point?
   # FIXME: add an error message
