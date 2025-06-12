2bb8cdcf2073c469add63a3fd4a3630a40d7458e
MKessar <astromouloud@gmail.com>
2025-06-12 10:33:26 +0200

    change name folders missons

--- missions/contrib/Intro/git04_alias_git_GB/auto.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-alias "$(gettext "journal")"="nano \"\$GSH_CHEST/$(gettext "journal").txt\""
-gsh check
--- missions/contrib/Intro/git04_alias_git_GB/static.sh
+++
@@ -1,1 +0,0 @@
-cp "$(eval_gettext '$MISSION_DIR/gshrc/en.sh')" "$GSH_CONFIG/player.gshrc"
