7ebb82d26518f97845587e5ad00489129f39fdfa
Matthieu Haefele <matthieu.haefele@cnrs.fr>
2025-06-20 18:39:18 +0200

    04 contrib ok

---
+++ missions/contrib/git_branching/04_contrib/static.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# This file is not required: it is sourced once when initialising a GameShell
+# game, and whenever the corresponding missions is (re)started.
+# It typically creates the parts of the mission that will be available during
+# the whole game, like the directory structure.
+#
+# Since it is sourced, it may define environment variables if you really need
+# them, but it should "unset" any local variable it has created.
+
+mkdir -p $GSH_HOME/Castle/Portals/
+mkdir -p $GSH_HOME/Castle/Main_tower/First_floor
+cp $MISSION_DIR/ArcaneBook $GSH_HOME/Castle/Main_tower/First_floor
+git config --global user.email "jaffar@aladin.cool"
+git config --global user.name "Jaffar"
+
