c2c67d488b9f80931d084e5d8c5ab7097438e28e
Matthieu Haefele <matthieu.haefele@cnrs.fr>
2025-06-20 17:25:38 +0200

    03 mergeFF ok + fix 02 static.sh

---
+++ missions/contrib/git_branching/03_mergeFF/check.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+# This file is required. It is sourced when checking the goal of the mission
+# has been achieved.
+# It should end with a command returning 0 on success, and something else on
+# failure.
+# It should "unset" any local variable it has created, and any "global
+# variable" that were only used for the mission. (The function _mission_check
+# is automatically unset.)
+#
+# It typically looks like
+
+_mission_check() {
+
+  goal=$GSH_HOME/Castle/Portals/al_far
+  current="$PWD"
+
+  if [ "$goal" != "$current" ] 
+    then 
+      echo "You are not in the Al Far portal"
+      return 1
+  fi
+
+  current_branch=$(git branch --show-current)
+  if [ "$current_branch" != "main" ]
+    then
+        echo "You are not on the main branch..."
+        return 1
+  fi
+
+  log=$(git branch --merged | grep "new_ingredient")
+  if [ "$log" = "" ]
+    then
+        echo "new_ingredient branch is not merged..."
+        return 1
+  fi
+
+  return 0
+
+}
+_mission_check
