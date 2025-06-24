568f3d45d9d539b16a3a8d9fd908302b88292df9
Jaffar <jaffar@aladin.cool>
2025-06-24 17:37:22 +0200

    05_merge ok

---
+++ missions/contrib/git_branching/05_merge/auto.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# This file is not required. When it exists, it is used to automatically
+# validate the mission. It should end with a succesful `gsh check` command.
+# It is sometimes possible to "cheat" by using any hidden data in $GSH_TMP,
+# but it is better to do it the "intended" way.
+# If you write this file, rename it to auto.sh
+
+git merge new_ingredient
+gsh check
+
