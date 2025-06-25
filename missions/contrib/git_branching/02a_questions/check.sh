25ddc0faed135d6f7283828cb0b2b429aec074fe
Jaffar <jaffar@aladin.cool>
2025-06-25 20:01:28 +0200

    02a_questions ok

--- missions/contrib/git_branching/02a_questions/check.sh
+++ missions/contrib/git_branching/02a_questions/check.sh
@@ -10,43 +10,72 @@
 #
 # It typically looks like
 
-_mission_check() {
-
-  goal=$GSH_HOME/Castle/Portals/al_far
-  current="$PWD"
-
-  if [ "$goal" != "$current" ] 
-    then 
-      echo "You are not in the Al Far portal"
-      return 1
-  fi
-
-  current_branch=$(git branch --show-current)
-  if [ "$current_branch" != "main" ]
-    then
-        echo "You are not on the main branch..."
-        return 1
-  fi
-
-
-echo "If we want to merge the new_ingredient branch into main, which kind of merge it is ?"
-echo "1. a fast-forward merge"
-echo "2. a real merge"
+check_answer() {
+result=$1
+error_str=$2
 read -r response
-echo $response
 case "$response" in
   "" | *[!0-9]*)
     echo "That's not even a number!"
     return 1
     ;;
   *)
-    if [ "$response" -ne "1" ]
+    if [ "$response" -ne $result ]
     then
-      echo "Too bad! The answer was 1. Indeed, it is a fast forward merge as there is a single direct link between the commit of the main branch and the commit pointed by the new_ingredient branch"
+      echo $error_str
       return 1
     fi
     ;;
 esac
+}
+
+_mission_check() {
+
+  goal=$GSH_HOME/Castle/Portals/al_far
+  current="$PWD"
+
+#  if [ "$goal" != "$current" ] 
+#    then 
+#      echo "You are not in the Al Far portal"
+#      return 1
+#  fi
+#
+#  current_branch=$(git branch --show-current)
+#  if [ "$current_branch" != "main" ]
+#    then
+#        echo "You are not on the main branch..."
+#        return 1
+#  fi
+
+
+echo "If we want to merge the new_ingredient branch into main, which kind of merge it is ?"
+echo "1. a fast-forward merge"
+echo "2. a real merge"
+res="1"
+err_str="Too bad! The answer was $res. Indeed, it is a fast forward merge as there is a single direct link between the commit of the main branch and the commit pointed by the new_ingredient branch"
+check_answer "$res" "$err_str"
+
+echo "If we want to merge the add_rune branch into main, which kind of merge it is ?"
+echo "1. a fast-forward merge"
+echo "2. a real merge"
+res="2"
+err_str="Too bad! The answer was $res. Indeed, it is a real merge because the history has diverged between the main branch and add_rune branch."
+check_answer "$res" "$err_str"
+
+echo "What is the first ingredient of the spell on the main branch (provide the number) ?"
+res="11"
+err_str="Too bad! The answer was $res."
+check_answer "$res" "$err_str"
+
+echo "What is the first ingredient of the spell on the new_ingredient branch (provide the number) ?"
+res="11"
+err_str="Too bad! The answer was $res."
+check_answer "$res" "$err_str"
+
+echo "What is the first ingredient of the spell on the add_rune branch (provide the number) ?"
+res="33"
+err_str="Too bad! The answer was $res."
+check_answer "$res" "$err_str"
 
 }
 _mission_check
