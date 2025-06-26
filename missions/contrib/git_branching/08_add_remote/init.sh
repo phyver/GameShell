015a8e722d372b02866bf867f7add3da8185dc8b
Jaffar <jaffar@aladin.cool>
2025-06-26 17:10:34 +0200

    08 add remote ok, missing auto still

---
+++ missions/contrib/git_branching/08_add_remote/init.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# This file is not required: it is sourced every time the mission is started.
+# Since it is sourced every time the mission is restarted, it can generate
+# random data to make each run slightly different.
+#
+# Since it is sourced, it may define environment variables if you really need
+# them, but it should "unset" any local variable it has created.
+#
+# Note however that should the mission be initialized in a subshell, those
+# environment variables will disappear! That typically happens a mission is
+# checked using process redirection, as in
+#   $ SOMETHING | gsh check
+# To mitigate the problem, GameShell will display a message asking the player
+# to run
+#   $ gsh reset
+# in that case.
+#
+# It typically looks like
+_mission_init() {
+  # ...
+  if ! git --version > /dev/null 2 > /dev/null
+    then  echo "git not found"
+    return 1
+  fi
+  cp $MISSION_DIR/PortalBook $GSH_HOME/Castle/Main_tower
+  
+}
+_mission_init
---
+++ missions/contrib/git_branching/08_add_remote/static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# This file is not required: it is sourced once when initialising a GameShell
+# game, and whenever the corresponding missions is (re)started.
+# It typically creates the parts of the mission that will be available during
+# the whole game, like the directory structure.
+#
+# Since it is sourced, it may define environment variables if you really need
+# them, but it should "unset" any local variable it has created.
+
+mkdir -p $GSH_HOME/Castle/Portals/
+mkdir -p $GSH_HOME/Castle/Main_tower
